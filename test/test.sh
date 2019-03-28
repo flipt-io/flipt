f2681d414a620a0c398a9ec68aa47d14e1306713
Mark Phelps <mark.aaron.phelps@gmail.com>
2019-03-27 21:45:26 -0400

    More tests

--- test/test.sh
+++ test/test.sh
@@ -4,7 +4,7 @@ source /vendor/shakedown/shakedown.sh
 
 uuid_str()
 {
-    cat /dev/urandom | tr -dc 'a-z' | fold -w 16 | head -n 1
+    cat /dev/urandom | tr -dc '[:lower:]' | fold -w 16 | head -n 1
 }
 
 step_1_test_health()
@@ -54,16 +54,23 @@ step_2_test_flags_and_variants()
     variant_key_1=$(uuid_str)
     variant_key_2=$(uuid_str)
 
-    shakedown POST "$flipt_url/flags/$flag_key/variants" -H 'Content-Type:application/json' -d "{\"key\": \"$variant_key_1\"}"
+    shakedown POST "$flipt_url/flags/$flag_key/variants" -H 'Content-Type:application/json' -d "{\"key\":\"$variant_key_1\"}"
         status 200
         matches "\"key\":\"$variant_key_1\""
 
-    shakedown POST "$flipt_url/flags/$flag_key/variants" -H 'Content-Type:application/json' -d "{\"key\": \"$variant_key_2\"}"
+    shakedown POST "$flipt_url/flags/$flag_key/variants" -H 'Content-Type:application/json' -d "{\"key\":\"$variant_key_2\"}"
         status 200
         matches "\"key\":\"$variant_key_2\""
+
+    # get flag w/ variants
+    shakedown GET "$flipt_url/flags/$flag_key" -H 'Content-Type:application/json'
+        status 200
+        matches "\"key\":\"$flag_key\""
+        contains "$variant_key_1"
+        contains "$variant_key_2"
 }
 
-step_4_test_segments_and_constraints()
+step_3_test_segments_and_constraints()
 {
     flipt_url=$1:8080/api/v1
 
@@ -95,9 +102,29 @@ step_4_test_segments_and_constraints()
         status 200
         matches "\"key\":\"$segment_key\""
         matches "\"name\":\"$name_2\""
+
+    # create constraints
+    shakedown POST "$flipt_url/segments/$segment_key/constraints" -H 'Content-Type:application/json' -d "{\"type\":\"STRING_COMPARISON_TYPE\",\"property\":\"foo\",\"operator\":\"eq\",\"value\":\"bar\"}"
+        status 200
+        matches "\"property\":\"foo\""
+        matches "\"operator\":\"eq\""
+        matches "\"value\":\"bar\""
+
+    shakedown POST "$flipt_url/segments/$segment_key/constraints" -H 'Content-Type:application/json' -d "{\"type\":\"STRING_COMPARISON_TYPE\",\"property\":\"fizz\",\"operator\":\"neq\",\"value\":\"buzz\"}"
+        status 200
+        matches "\"property\":\"fizz\""
+        matches "\"operator\":\"neq\""
+        matches "\"value\":\"buzz\""
+
+    # get segment w/ constraints
+    shakedown GET "$flipt_url/segments/$segment_key" -H 'Content-Type:application/json'
+        status 200
+        matches "\"key\":\"$segment_key\""
+        contains "foo"
+        contains "fizz"
 }
 
-step_10_test_delete() {
+step_4_test_delete() {
     # delete flag
     shakedown DELETE "$flipt_url/flags/$flag_key" -H 'Content-Type:application/json'
         status 200
@@ -119,8 +146,8 @@ start()
 
     step_1_test_health "$flipt_host"
     step_2_test_flags_and_variants "$flipt_host"
-    step_4_test_segments_and_constraints "$flipt_host"
-    step_10_test_delete "$flipt_host"
+    step_3_test_segments_and_constraints "$flipt_host"
+    step_4_test_delete "$flipt_host"
 }
 
 start flipt
