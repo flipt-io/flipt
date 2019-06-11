8ad93e260be2faaf56a66cc7113abf29d2901b77
Mark Phelps <mark.aaron.phelps@gmail.com>
2019-06-10 21:31:48 -0400

    Bump go versions in Dockerfiles and dev machines

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.12.4-alpine AS build
+FROM golang:1.12.5-alpine AS build
 
 RUN apk add --no-cache \
     gcc \
--- build/dev/centos/setup.sh
+++ build/dev/centos/setup.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 NODE_VERSION='12.4.0'
 YARN_VERSION='1.16.0'
-GO_VERSION='1.12.4'
+GO_VERSION='1.12.5'
 PROTOC_VERSION='3.6.1'
 
 echo '=== Installing necessary system libraries to build'
