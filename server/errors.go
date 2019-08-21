7dab5d3fa9331ce54cd60c09eec52bec0e411024
Mark Phelps <mark.aaron.phelps@gmail.com>
2019-08-21 10:57:48 -0400

    Revert "WIP https"
    This reverts commit c006d45a644fa3a2c0650298dc117103950bbb8e.

--- server/errors.go
+++ server/errors.go
@@ -12,12 +12,12 @@ func (e errInvalidField) Error() string {
 	return fmt.Sprintf("invalid field %s: %s", e.field, e.reason)
 }
 
-// invalidFieldError creates an errInvalidField for a specific field and reason
+// invalidFieldError creates an ErrInvalidField for a specific field and reason
 func invalidFieldError(field, reason string) error {
 	return errInvalidField{field, reason}
 }
 
-// emptyFieldError creates an errInvalidField for an empty field
+// emptyFieldError creates an ErrInvalidField for an empty field
 func emptyFieldError(field string) error {
 	return invalidFieldError(field, "must not be empty")
 }
