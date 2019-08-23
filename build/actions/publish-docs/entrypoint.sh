221ffe055996dd6fb77f3953fe8847fe5335eda4
Mark Phelps <mark.aaron.phelps@gmail.com>
2019-08-23 12:02:44 -0400

    Try to use GITHUB_ACTOR env var

--- build/actions/publish-docs/entrypoint.sh
+++ build/actions/publish-docs/entrypoint.sh
@@ -2,8 +2,13 @@
 
 set -eu
 
-git config user.name "Mark Phelps"
-git config user.email "mark.aaron.phelps@gmail.com"
-git remote add gh-token "https://${GITHUB_TOKEN}@github.com/markphelps/flipt.git"
+if [ -z "${GITHUB_TOKEN}" ]; then
+    echo "error: not found GITHUB_TOKEN"
+    exit 1
+fi
+
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+git remote add gh-token "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/markphelps/flipt.git"
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages
 mkdocs gh-deploy --clean --remote-name gh-token
