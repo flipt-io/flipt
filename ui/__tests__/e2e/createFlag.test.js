de171aff4243dd89b48ec81b80d13c0031922888
Mark Phelps <mark.aaron.phelps@gmail.com>
2021-06-11 09:15:43 -0400

    Expect playwright

--- ui/__tests__/e2e/createFlag.test.js
+++ ui/__tests__/e2e/createFlag.test.js
@@ -1,4 +1,5 @@
-const { chromium } = require('playwright');
+const { chromium } = require("playwright");
+import expect from "expect-playwright";
 
 let browser;
 let page;
@@ -18,18 +19,26 @@ test("createFlag", async () => {
   await page.goto(addr);
   await page.click("[data-testid='new-flag']");
   await page.type("[placeholder='Flag name']", "Awesome new feature");
-  await page.type("[placeholder='Flag description']", "Our product manager cannot wait to ship this!");
+  await page.type(
+    "[placeholder='Flag description']",
+    "Our product manager cannot wait to ship this!"
+  );
   await page.click("[data-testid='create-flag']");
   await page.click('[aria-label="breadcrumbs"] .router-link-active');
   await expect(page).toHaveText("awesome-new-feature");
   await page.click('[href="#/flags/awesome-new-feature"]');
 });
 
-test('createFlagDisallowSpecialChars', async () => {
+test("createFlagDisallowSpecialChars", async () => {
   await page.goto(addr);
   await page.click("[data-testid='new-flag']");
   await page.type("[placeholder='Flag name']", "My flag with colons");
   await page.type("[placeholder='Flag key']", "colons:are:not:allowed");
-  await page.type("[placeholder='Flag description']", "This should not be saved");
-  await expect(page).toHaveText("Only letters, numbers, hypens and underscores allowed");
+  await page.type(
+    "[placeholder='Flag description']",
+    "This should not be saved"
+  );
+  await expect(page).toHaveText(
+    "Only letters, numbers, hypens and underscores allowed"
+  );
 });
--- ui/__tests__/e2e/createSegment.test.js
+++ ui/__tests__/e2e/createSegment.test.js
@@ -1,4 +1,5 @@
-const { chromium } = require('playwright');
+const { chromium } = require("playwright");
+import expect from "expect-playwright";
 
 let browser;
 let page;
@@ -21,19 +22,27 @@ test("createSegment", async () => {
   await page.click("[placeholder='Segment name']");
   await page.type("[placeholder='Segment name']", "Power users");
   await page.click("[placeholder='Segment description']");
-  await page.type("[placeholder='Segment description']", "Users that are willing to try out advanced functionality");
+  await page.type(
+    "[placeholder='Segment description']",
+    "Users that are willing to try out advanced functionality"
+  );
   await page.click("[data-testid='create-segment']");
   await page.click('[aria-label="breadcrumbs"] .router-link-active');
   await expect(page).toHaveText("power-users");
   await page.click('[href="#/segments/power-users"]');
 });
 
-test('createSegmentDisallowSpecialChars', async () => {
+test("createSegmentDisallowSpecialChars", async () => {
   await page.goto(addr);
   await page.click("[data-testid='segments']");
   await page.click("[data-testid='new-segment']");
   await page.type("[placeholder='Segment name']", "My segment with colons");
   await page.type("[placeholder='Segment key']", "colons:are:not:allowed");
-  await page.type("[placeholder='Segment description']", "This should not be saved");
-  await expect(page).toHaveText("Only letters, numbers, hypens and underscores allowed");
+  await page.type(
+    "[placeholder='Segment description']",
+    "This should not be saved"
+  );
+  await expect(page).toHaveText(
+    "Only letters, numbers, hypens and underscores allowed"
+  );
 });
