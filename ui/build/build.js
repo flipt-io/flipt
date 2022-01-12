7715c650696bdba9c2b948b059a580d3e8bad633
Mark Phelps <mark.aaron.phelps@gmail.com>
2022-01-12 09:45:36 -0500

    Upgrade to ES syntax or something idk; also modify some goreleaser settings for release

--- ui/build/build.js
+++ ui/build/build.js
@@ -1,41 +1,45 @@
-'use strict'
-require('./check-versions')()
+"use strict";
+require("./check-versions")();
 
-process.env.NODE_ENV = 'production'
+process.env.NODE_ENV = "production";
 
-const ora = require('ora')
-const rm = require('rimraf')
-const path = require('path')
-const chalk = require('chalk')
-const webpack = require('webpack')
-const config = require('../config')
-const webpackConfig = require('./webpack.prod.conf')
+import ora from "ora";
+import rm from "rimraf";
+import { join } from "path";
+import { red, cyan, yellow } from "chalk";
+import webpack from "webpack";
+import { build } from "../config";
+import webpackConfig from "./webpack.prod.conf";
 
-const spinner = ora('building for production...')
-spinner.start()
+const spinner = ora("building for production...");
+spinner.start();
 
-rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
-  if (err) throw err
+rm(join(build.assetsRoot, build.assetsSubDirectory), (err) => {
+  if (err) throw err;
   webpack(webpackConfig, (err, stats) => {
-    spinner.stop()
-    if (err) throw err
-    process.stdout.write(stats.toString({
-      colors: true,
-      modules: false,
-      children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
-      chunks: false,
-      chunkModules: false
-    }) + '\n\n')
+    spinner.stop();
+    if (err) throw err;
+    process.stdout.write(
+      stats.toString({
+        colors: true,
+        modules: false,
+        children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
+        chunks: false,
+        chunkModules: false,
+      }) + "\n\n"
+    );
 
     if (stats.hasErrors()) {
-      console.log(chalk.red('  Build failed with errors.\n'))
-      process.exit(1)
+      console.log(red("  Build failed with errors.\n"));
+      process.exit(1);
     }
 
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
-  })
-})
+    console.log(cyan("  Build complete.\n"));
+    console.log(
+      yellow(
+        "  Tip: built files are meant to be served over an HTTP server.\n" +
+          "  Opening index.html over file:// won't work.\n"
+      )
+    );
+  });
+});
