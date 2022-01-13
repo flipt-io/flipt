88e113b0fdaafd9be5ce136a21a4a2f5f0966113
Mark Phelps <mark.aaron.phelps@gmail.com>
2022-01-13 15:01:08 +0000

    Add Footer wip; recommended extentions

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
-    <Nav />
-    <RouterView />
+    <div id="wrapper">
+      <Nav />
+      <RouterView />
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Footer from "./components/Footer.vue";
 import Nav from "./components/Nav";
 
 export default {
   name: "App",
   components: {
     Nav,
+    Footer,
   },
 };
 </script>
---
+++ ui/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">I am down here.</div>
+  </footer>
+</template>
+
+<script>
+export default {
+  Name: "footer",
+  data() {
+    return {
+      updateAvailable: false,
+      version: "dev",
+      latestVersion: "",
+    };
+  },
+};
+</script>
