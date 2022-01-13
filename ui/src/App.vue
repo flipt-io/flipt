b6dfd2df1b398e88d1341066b020bca87b65afbc
Mark Phelps <markphelps@github.com>
2022-01-13 22:27:13 +0000

    WIP

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -2,16 +2,19 @@
   <div>
     <Nav />
     <RouterView />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 export default {
   name: "App",
   components: {
     Nav,
+    Footer,
   },
 };
 </script>
---
+++ ui/src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Flipt</strong> version
+        <a>{{ version }}</a>
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  Name: "nav",
+  data() {
+    return {
+      hasUpdate: false,
+      version: "dev",
+      latestVersion: "",
+    };
+  },
+};
+</script>
