051029de6510938451eefe40d1a0a8d595b2bcda
Mark Phelps <mark.aaron.phelps@gmail.com>
2022-01-26 01:39:16 +0000

    pipefail smh

--- test/api.sh
+++ test/api.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 cd "$(dirname "$0")/.." || exit
 
 source ./test/helpers/shakedown/shakedown.sh
--- test/ui.sh
+++ test/ui.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 set -euo pipefail
 
 cd "$(dirname "$0")/.." || exit
