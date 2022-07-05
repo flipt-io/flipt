83a97e203470a6a2bb45f96a8bacc38137954860
Mark Phelps <mark@flipt.io>
2022-07-05 09:48:45 -0400

    Add filter func

--- ui/src/main.js
+++ ui/src/main.js
@@ -11,6 +11,16 @@ Vue.config.productionTip = false;
 Vue.use(VueMoment);
 Vue.use(Buefy);
 
+Vue.filter("limit", function (value) {
+  if (!value) return "";
+  value = value.toString();
+  if (value.length > 30) {
+    return value.substring(0, 29) + "...";
+  } else {
+    return value;
+  }
+});
+
 new Vue({
   router,
   store,
