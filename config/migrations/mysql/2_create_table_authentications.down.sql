391e4644786ce084fbe11bedc9411a272f9608c0
George MacRorie <me@georgemac.com>
2022-10-26 17:16:43 +0100

    chore(migrations): drop index before dropping authentications table

--- config/migrations/mysql/2_create_table_authentications.down.sql
+++ config/migrations/mysql/2_create_table_authentications.down.sql
@@ -1,1 +1,2 @@
+DROP INDEX hashed_client_token_authentications_index ON authentications;
 DROP TABLE IF EXISTS authentications;
--- config/migrations/postgres/4_create_table_authentications.down.sql
+++ config/migrations/postgres/4_create_table_authentications.down.sql
@@ -1,1 +1,2 @@
+DROP INDEX hashed_client_token_authentications_index;
 DROP TABLE IF EXISTS authentications;
