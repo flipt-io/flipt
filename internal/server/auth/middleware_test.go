ae2b176d9ae1efcd24c6a1614cf821509e626c37
George MacRorie <me@georgemac.com>
2022-11-01 13:08:13 +0000

    feat(server/auth): define unary server interceptor

---
+++ internal/server/auth/middleware_test.go
@@ -0,0 +1,97 @@
+package auth
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.flipt.io/flipt/internal/storage"
+	"go.flipt.io/flipt/internal/storage/auth/memory"
+	authrpc "go.flipt.io/flipt/rpc/flipt/auth"
+	"go.uber.org/zap/zaptest"
+	"google.golang.org/grpc/metadata"
+)
+
+func TestUnaryInterceptor(t *testing.T) {
+	authenticator := memory.NewStore()
+	clientToken, storedAuth, err := authenticator.CreateAuthentication(
+		context.TODO(),
+		&storage.CreateAuthenticationRequest{Method: authrpc.Method_TOKEN},
+	)
+	require.NoError(t, err)
+
+	for _, test := range []struct {
+		name         string
+		metadata     metadata.MD
+		expectedErr  error
+		expectedAuth *authrpc.Authentication
+	}{
+		{
+			name: "successful authentication",
+			metadata: metadata.MD{
+				"Authorization": []string{"Bearer " + clientToken},
+			},
+			expectedAuth: storedAuth,
+		},
+		{
+			name: "client token not found in store",
+			metadata: metadata.MD{
+				"Authorization": []string{"Bearer unknowntoken"},
+			},
+			expectedErr: errUnauthenticated,
+		},
+		{
+			name: "client token missing Bearer prefix",
+			metadata: metadata.MD{
+				"Authorization": []string{clientToken},
+			},
+			expectedErr: errUnauthenticated,
+		},
+		{
+			name: "authorization header empty",
+			metadata: metadata.MD{
+				"Authorization": []string{},
+			},
+			expectedErr: errUnauthenticated,
+		},
+		{
+			name:        "authorization header not set",
+			metadata:    metadata.MD{},
+			expectedErr: errUnauthenticated,
+		},
+		{
+			name:        "no metadata on context",
+			metadata:    nil,
+			expectedErr: errUnauthenticated,
+		},
+	} {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			var (
+				logger = zaptest.NewLogger(t)
+
+				ctx          = context.Background()
+				retrievedCtx = ctx
+				handler      = func(ctx context.Context, req interface{}) (interface{}, error) {
+					// update retrievedCtx to the one delegated to the handler
+					retrievedCtx = ctx
+					return nil, nil
+				}
+			)
+
+			if test.metadata != nil {
+				ctx = metadata.NewIncomingContext(ctx, test.metadata)
+			}
+
+			_, err := UnaryInterceptor(logger, authenticator)(
+				ctx,
+				nil,
+				nil,
+				handler,
+			)
+			require.Equal(t, test.expectedErr, err)
+			assert.Equal(t, test.expectedAuth, GetAuthenticationFrom(retrievedCtx))
+		})
+	}
+}
