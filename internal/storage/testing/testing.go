15808703c4241201689660383c2f19f8c0da3eec
George MacRorie <me@georgemac.com>
2022-11-07 09:58:39 +0000

    chore(storage/testing): use authCount constant variable in loop condition

--- internal/storage/testing/testing.go
+++ internal/storage/testing/testing.go
@@ -24,7 +24,7 @@ func TestAuthenticationStoreHarness(t *testing.T, fn func(t *testing.T) storage.
 	)
 
 	t.Run(fmt.Sprintf("Create %d authentications", authCount), func(t *testing.T) {
-		for i := 0; i < 100; i++ {
+		for i := 0; i < authCount; i++ {
 			token, auth, err := store.CreateAuthentication(ctx, &storage.CreateAuthenticationRequest{
 				Method: auth.Method_TOKEN,
 				Metadata: map[string]string{
