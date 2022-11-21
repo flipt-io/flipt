3af6474d97ea70694a24abcc35d2181c285a28da
Mark Phelps <209477+markphelps@users.noreply.github.com>
2022-11-21 13:47:54 -0500

    chore: rm temp var

--- internal/server/middleware_http.go
+++ internal/server/middleware_http.go
@@ -8,8 +8,7 @@ func StripSlashes(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
 		if len(path) > 1 && path[len(path)-1] == '/' {
-			newPath := path[:len(path)-1]
-			r.URL.Path = newPath
+			r.URL.Path = path[:len(path)-1]
 		}
 		next.ServeHTTP(w, r)
 	}
