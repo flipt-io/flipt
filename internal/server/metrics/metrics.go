062fcc8964ddbd8c179b0b246c7be7f7aaf39de0
Mark Phelps <209477+markphelps@users.noreply.github.com>
2022-11-23 11:33:16 -0500

    chore: try to appease the linter

--- internal/server/metrics/metrics.go
+++ internal/server/metrics/metrics.go
@@ -16,34 +16,43 @@ const (
 )
 
 // Prometheus metrics used throughout the server package
-var ErrorsTotal = metrics.MustSyncInt64().
-	Counter(
-		prometheus.BuildFQName(namespace, serverSubsystem, "errors"),
-		instrument.WithDescription("The total number of server errors"),
-	)
+var (
+	// ErrorsTotal is the total number of errors in the server
+	ErrorsTotal = metrics.MustSyncInt64().
+		Counter(
+			prometheus.BuildFQName(namespace, serverSubsystem, "errors"),
+			instrument.WithDescription("The total number of server errors"),
+		)
+)
 
 // Evaluation specific metrics
 var (
+	// EvaluationsTotal is the total number of evaluation requests
 	EvaluationsTotal = metrics.MustSyncInt64().
 				Counter(
 			prometheus.BuildFQName(namespace, evaluationsSubsystem, "requests"),
 			instrument.WithDescription("The total number of requested evaluations"),
 		)
 
+	// EvaluationErrorsTotal is the total number of evaluation errors
 	EvaluationErrorsTotal = metrics.MustSyncInt64().
 				Counter(
 			prometheus.BuildFQName(namespace, evaluationsSubsystem, "errors"),
 			instrument.WithDescription("The total number of requested evaluations"),
 		)
 
+	// EvaluationResultsTotal is the total number of evaluation results
 	EvaluationResultsTotal = metrics.MustSyncInt64().
 				Counter(
 			prometheus.BuildFQName(namespace, evaluationsSubsystem, "results"),
 			instrument.WithDescription("Count of results including match, flag, segment, reason and value attributes"),
 		)
 
+	// EvaluationLatency is the latency of individual evaluations
 	EvaluationLatency syncfloat64.Histogram
 
+	// Attributes used in evaluation metrics
+	//nolint
 	AttributeMatch   = attribute.Key("match")
 	AttributeFlag    = attribute.Key("flag")
 	AttributeSegment = attribute.Key("segment")
