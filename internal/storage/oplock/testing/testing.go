7ae9b1ddd3dbbac1399f300af8ab33eb9684457a
George MacRorie <me@georgemac.com>
2022-11-25 13:39:50 +0000

    chore(storage/oplock): re-define test harness to be deterministic

--- internal/storage/oplock/testing/testing.go
+++ internal/storage/oplock/testing/testing.go
@@ -2,7 +2,6 @@ package testing
 
 import (
 	"context"
-	"sync/atomic"
 	"testing"
 	"time"
 
@@ -14,12 +13,16 @@ import (
 
 // Harness is a test harness for all implementations of oplock.Service.
 // The test consists of firing multiple goroutines which attempt to acquire
-// a lock over a single operation "test". If acquired they increment a counter.
-// After attempting to acquire the lock each goroutine sleeps until the lock
-// can be attempted again.
+// a lock over a single operation "test".
+// Each acquisitions timestamp is pushed down a channel.
+// When five lock acquisitions have occurred the test ensures that it took
+// at-least a specified duration to do so (interval * (iterations - 1)).
+// Also that acquisitions occurred in ascending timestamp order with a delta
+// between each tick of at-least the configured interval.
 func Harness(t *testing.T, s oplock.Service) {
 	var (
-		count       int64
+		acquiredAt  = make(chan time.Time, 1)
+		interval    = 2 * time.Second
 		op          = oplock.Operation("test")
 		ctx, cancel = context.WithCancel(context.Background())
 	)
@@ -27,32 +30,60 @@ func Harness(t *testing.T, s oplock.Service) {
 	errgroup, ctx := errgroup.WithContext(ctx)
 
 	for i := 0; i < 5; i++ {
+		var acquiredUntil = time.Now().UTC()
+
 		errgroup.Go(func() error {
 			for {
 				select {
 				case <-ctx.Done():
 					return nil
-				default:
+				case <-time.After(time.Until(acquiredUntil)):
 				}
 
-				acquired, entry, err := s.TryAcquire(ctx, op, 2*time.Second)
+				acquired, entry, err := s.TryAcquire(ctx, op, interval)
 				require.NoError(t, err)
 
 				if acquired {
-					atomic.AddInt64(&count, 1)
+					acquiredAt <- entry.LastAcquired
 				}
 
-				time.Sleep(time.Until(entry.AcquiredUntil))
+				acquiredUntil = entry.AcquiredUntil
 			}
 		})
 	}
 
-	<-time.After(10 * time.Second)
+	now := time.Now().UTC()
+	var acquisitions []time.Time
+	for tick := range acquiredAt {
+		acquisitions = append(acquisitions, tick)
+
+		if len(acquisitions) == 5 {
+			break
+		}
+	}
+
+	since := time.Since(now)
+
+	// ensure it took at-least 8s second to acquire 5 locks
+	require.Greater(t, since, 8*time.Second)
+
+	t.Logf("It took %s to consume the lock 5 times with an interval of %s\n", since, interval)
 
 	cancel()
 
 	require.NoError(t, errgroup.Wait())
 
-	// ensure counter only incremented 5 times in 5 seconds
-	assert.Equal(t, int64(5), count)
+	close(acquiredAt)
+
+	// ensure ticks were acquired sequentially
+	assert.IsIncreasing(t, acquisitions)
+
+	for i, tick := range acquisitions {
+		if len(acquisitions) == i+1 {
+			break
+		}
+
+		// tick at T(n+1) occurs at-least <interval> after T(n)
+		assert.Greater(t, acquisitions[i+1].Sub(tick), interval)
+	}
 }
