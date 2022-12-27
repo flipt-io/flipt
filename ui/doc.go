6f6ce65587bf61a9887cb777e296336d7ab4b138
Mark Phelps <209477+markphelps@users.noreply.github.com>
2022-12-27 10:20:59 -0500

    feat: work on adding uiv2

--- ui/doc.go
+++
@@ -1,8 +0,0 @@
-//go:build !assets
-// +build !assets
-
-package ui
-
-import "embed"
-
-var UI embed.FS
--- ui/embed.go
+++
@@ -1,9 +0,0 @@
-//go:build assets
-// +build assets
-
-package ui
-
-import "embed"
-
-//go:embed dist/*
-var UI embed.FS
