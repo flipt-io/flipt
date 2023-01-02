bafca339bab493deb322edaa1b74a478894e3567
George MacRorie <me@georgemac.com>
2023-01-02 18:11:35 +0000

    feat(ui): propagate csrf token from meta endpoints

--- ui/src/services/api.js
+++ ui/src/services/api.js
@@ -1,5 +1,19 @@
 import axios from "axios";
+import store from "../store";
+
+const CSRFTokenHeader = 'X-CSRF-Token';
 
 export const Api = axios.create({
   baseURL: "/api/v1/",
+});
+
+Api.interceptors.request.use((config) => {
+  const token = store.getters.csrfToken;
+  if (token != null) {
+    config.headers[CSRFTokenHeader] = token;
+  }
+
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
