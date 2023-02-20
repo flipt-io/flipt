9c3cab439846ad339a0a9aa73574f0d05849246e
George <me@georgemac.com>
2023-02-20 18:04:24 +0000

    feat(auth/k8s): kubernetes auth method service implementation (#1344)
    * feat(auth/k8s): initial server implementation and integration test
    
    * test(auth/k8s): happy path unit test case for server VerifyServiceAccount
    
    * test(auth/k8s): ensure verify uses expiration time from token claims
    
    * feat(auth/k8s): verify service account adapts verified claims to client token
    
    * chore(auth/k8s): changed wording in test skip
    
    * feat(auth/k8s): implement OIDC-based token verification
    
    * chore(auth/k8s): add documentation around VerifyServiceAccount
    
    * chore(auth/k8s): set TLS min version to 1.2
    
    * fix(config/auth/k8s): change issuer and ca path defaults
    
    * feat(auth/k8s): install new kubernetes auth method into grpc and http servers
    
    * fix(auth/k8s): use discovery endpoint issuer when verifiying SA tokens
    
    * fix(auth/k8s): thread context when making discovery endpoint request

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -497,8 +497,8 @@ func TestLoad(t *testing.T) {
 					Kubernetes: AuthenticationMethod[AuthenticationMethodKubernetesConfig]{
 						Enabled: true,
 						Method: AuthenticationMethodKubernetesConfig{
-							IssuerURL:               "https://kubernetes.default.svc",
-							CAPath:                  "/var/run/secrets/kubernetes.io/serviceaccount/ca.cert",
+							DiscoveryURL:            "https://kubernetes.default.svc.cluster.local",
+							CAPath:                  "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt",
 							ServiceAccountTokenPath: "/var/run/secrets/kubernetes.io/serviceaccount/token",
 						},
 						Cleanup: &AuthenticationCleanupSchedule{
@@ -608,7 +608,7 @@ func TestLoad(t *testing.T) {
 						Kubernetes: AuthenticationMethod[AuthenticationMethodKubernetesConfig]{
 							Enabled: true,
 							Method: AuthenticationMethodKubernetesConfig{
-								IssuerURL:               "https://some-other-k8s.namespace.svc",
+								DiscoveryURL:            "https://some-other-k8s.namespace.svc",
 								CAPath:                  "/path/to/ca/certificate/ca.pem",
 								ServiceAccountTokenPath: "/path/to/sa/token",
 							},
---
+++ internal/server/auth/method/kubernetes/testing/http.go
@@ -0,0 +1,49 @@
+package testing
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/stretchr/testify/require"
+	"go.flipt.io/flipt/internal/config"
+	"go.flipt.io/flipt/internal/gateway"
+	"go.flipt.io/flipt/rpc/flipt/auth"
+	"go.uber.org/zap"
+)
+
+type HTTPServer struct {
+	*GRPCServer
+}
+
+func StartHTTPServer(
+	t *testing.T,
+	ctx context.Context,
+	logger *zap.Logger,
+	conf config.AuthenticationConfig,
+	router chi.Router,
+) *HTTPServer {
+	t.Helper()
+
+	var (
+		mux        = gateway.NewGatewayServeMux()
+		httpServer = &HTTPServer{
+			GRPCServer: StartGRPCServer(t, ctx, logger, conf),
+		}
+	)
+
+	err := auth.RegisterAuthenticationMethodKubernetesServiceHandler(
+		ctx,
+		mux,
+		httpServer.GRPCServer.ClientConn,
+	)
+	require.NoError(t, err)
+
+	router.Mount("/auth/v1", mux)
+
+	return httpServer
+}
+
+func (s *HTTPServer) Stop() error {
+	return s.GRPCServer.Stop()
+}
