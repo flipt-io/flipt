2c6316ee2919bb27000a9c97aa0ea1144583a194
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-02-27 15:55:16 -0500

    chore: just use TODO for now for down migration

--- config/migrations/sqlite3/6_create_namespaces.down.sql
+++ config/migrations/sqlite3/6_create_namespaces.down.sql
@@ -1,7 +1,1 @@
-DROP INDEX flags_namespace_key_idx;
-DROP INDEX segments_namespace_key_idx;
-
-ALTER TABLE flags DROP COLUMN namespace_key;
-ALTER TABLE segments DROP COLUMN namespace_key;
-
-DROP TABLE IF EXISTS namespaces;
+/* TODO */
