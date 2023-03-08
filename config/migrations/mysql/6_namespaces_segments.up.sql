bbbbce3de92a31a9e7e488e159ab2063d81e35e1
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-03-08 10:58:30 -0500

    chore: add migration comments, revert local config

--- config/migrations/mysql/6_namespaces_segments.up.sql
+++ config/migrations/mysql/6_namespaces_segments.up.sql
@@ -5,6 +5,6 @@ ALTER TABLE segments DROP PRIMARY KEY, ADD PRIMARY KEY (`namespace_key`, `key`);
 
 ALTER TABLE constraints ADD COLUMN namespace_key VARCHAR(255) NOT NULL DEFAULT 'default';
 ALTER TABLE constraints DROP FOREIGN KEY `constraints_ibfk_1`; /* drop previously created foreign key */
-ALTER TABLE constraints DROP INDEX `segment_key` , ADD INDEX `constraints_namespace_segment_key` (`namespace_key`, `segment_key`) USING BTREE;
+ALTER TABLE constraints DROP INDEX `segment_key`, ADD INDEX `constraints_namespace_segment_key` (`namespace_key`, `segment_key`) USING BTREE; /* drop previously created index */
 ALTER TABLE constraints ADD FOREIGN KEY (namespace_key) REFERENCES namespaces(`key`) ON DELETE CASCADE;
 ALTER TABLE constraints ADD FOREIGN KEY (namespace_key, segment_key) REFERENCES segments(`namespace_key`, `key`) ON DELETE CASCADE;
