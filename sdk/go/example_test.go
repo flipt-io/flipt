b2252b79546c9e20e75b6c2c8dde918b7cc68854
George MacRorie <me@georgemac.com>
2023-03-13 09:31:54 +0000

    fix(sdk/go): documentation and example corrections

--- sdk/go/example_test.go
+++ sdk/go/example_test.go
@@ -2,6 +2,8 @@ package sdk
 
 import (
 	context "context"
+
+	flipt "go.flipt.io/flipt/rpc/flipt"
 )
 
 func ExampleNew() {
@@ -11,5 +13,5 @@ func ExampleNew() {
 
 	client := New(transport)
 
-	client.Flipt().GetFlag(context.Background(), "my_flag")
+	client.Flipt().GetFlag(context.Background(), &flipt.GetFlagRequest{Key: "my_flag"})
 }
