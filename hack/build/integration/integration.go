7558220549f6801fb093d33ef26f62f8f33cbfb1
George MacRorie <me@georgemac.com>
2023-03-17 11:34:11 +0000

    test(hack/build/integration): update flag case

--- hack/build/integration/integration.go
+++ hack/build/integration/integration.go
@@ -15,19 +15,35 @@ func Harness(t *testing.T, fn func(t *testing.T) sdk.SDK) {
 
 	ctx := context.Background()
 
+	t.Log("Create a new flag with key \"test\".")
+
 	created, err := client.Flipt().CreateFlag(ctx, &flipt.CreateFlagRequest{
 		Key:         "test",
 		Name:        "Test",
 		Description: "This is a test flag",
+		Enabled:     true,
 	})
 	require.NoError(t, err)
 
 	assert.Equal(t, "test", created.Key)
 	assert.Equal(t, "Test", created.Name)
 	assert.Equal(t, "This is a test flag", created.Description)
+	assert.True(t, created.Enabled, "Flag should be enabled")
+
+	t.Log("Retrieve flag with key \"test\".")
 
 	flag, err := client.Flipt().GetFlag(ctx, &flipt.GetFlagRequest{Key: "test"})
 	require.NoError(t, err)
 
 	assert.Equal(t, created, flag)
+
+	t.Log("Update flag with key \"test\".")
+
+	updated, err := client.Flipt().UpdateFlag(ctx, &flipt.UpdateFlagRequest{
+		Key:  "test",
+		Name: "Test 2",
+	})
+	require.NoError(t, err)
+
+	assert.Equal(t, "Test 2", updated.Name)
 }
