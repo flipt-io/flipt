d0d9c84460872c4dae2d75ff3480a0d5dc51b9d0
George MacRorie <me@georgemac.com>
2023-03-17 12:01:26 +0000

    test(hack/build/integration): create segment case

--- hack/build/integration/integration.go
+++ hack/build/integration/integration.go
@@ -15,79 +15,94 @@ func Harness(t *testing.T, fn func(t *testing.T) sdk.SDK) {
 
 	ctx := context.Background()
 
-	t.Log("Create a new flag with key \"test\".")
+	t.Run("Flags and Variants", func(t *testing.T) {
+		t.Log("Create a new flag with key \"test\".")
 
-	created, err := client.Flipt().CreateFlag(ctx, &flipt.CreateFlagRequest{
-		Key:         "test",
-		Name:        "Test",
-		Description: "This is a test flag",
-		Enabled:     true,
-	})
-	require.NoError(t, err)
-
-	assert.Equal(t, "test", created.Key)
-	assert.Equal(t, "Test", created.Name)
-	assert.Equal(t, "This is a test flag", created.Description)
-	assert.True(t, created.Enabled, "Flag should be enabled")
-
-	t.Log("Retrieve flag with key \"test\".")
-
-	flag, err := client.Flipt().GetFlag(ctx, &flipt.GetFlagRequest{Key: "test"})
-	require.NoError(t, err)
-
-	assert.Equal(t, created, flag)
-
-	t.Log("Update flag with key \"test\".")
-
-	updated, err := client.Flipt().UpdateFlag(ctx, &flipt.UpdateFlagRequest{
-		Key:  "test",
-		Name: "Test 2",
-	})
-	require.NoError(t, err)
-
-	assert.Equal(t, "Test 2", updated.Name)
-
-	t.Log("List all flags.")
-
-	flags, err := client.Flipt().ListFlags(ctx, &flipt.ListFlagRequest{})
-	require.NoError(t, err)
-
-	assert.Len(t, flags.Flags, 1)
-	assert.Equal(t, updated.Key, flags.Flags[0].Key)
-	assert.Equal(t, updated.Name, flags.Flags[0].Name)
-	assert.Equal(t, updated.Description, flags.Flags[0].Description)
-
-	for _, key := range []string{"one", "two"} {
-		t.Logf("Create variant with key %q.", key)
-
-		createdVariant, err := client.Flipt().CreateVariant(ctx, &flipt.CreateVariantRequest{
-			Key:     key,
-			Name:    key,
-			FlagKey: "test",
+		created, err := client.Flipt().CreateFlag(ctx, &flipt.CreateFlagRequest{
+			Key:         "test",
+			Name:        "Test",
+			Description: "This is a test flag",
+			Enabled:     true,
 		})
 		require.NoError(t, err)
 
-		assert.Equal(t, key, createdVariant.Key)
-		assert.Equal(t, key, createdVariant.Name)
-	}
+		assert.Equal(t, "test", created.Key)
+		assert.Equal(t, "Test", created.Name)
+		assert.Equal(t, "This is a test flag", created.Description)
+		assert.True(t, created.Enabled, "Flag should be enabled")
 
-	t.Log("Get flag \"test\" and check variants.")
+		t.Log("Retrieve flag with key \"test\".")
 
-	flag, err = client.Flipt().GetFlag(ctx, &flipt.GetFlagRequest{Key: "test"})
-	require.NoError(t, err)
+		flag, err := client.Flipt().GetFlag(ctx, &flipt.GetFlagRequest{Key: "test"})
+		require.NoError(t, err)
 
-	assert.Len(t, flag.Variants, 2)
+		assert.Equal(t, created, flag)
 
-	t.Log("Update variant \"one\" (rename from \"one\" to \"One\")")
+		t.Log("Update flag with key \"test\".")
 
-	updatedVariant, err := client.Flipt().UpdateVariant(ctx, &flipt.UpdateVariantRequest{
-		FlagKey: "test",
-		Id:      flag.Variants[0].Id,
-		Key:     "one",
-		Name:    "One",
+		updated, err := client.Flipt().UpdateFlag(ctx, &flipt.UpdateFlagRequest{
+			Key:  "test",
+			Name: "Test 2",
+		})
+		require.NoError(t, err)
+
+		assert.Equal(t, "Test 2", updated.Name)
+
+		t.Log("List all flags.")
+
+		flags, err := client.Flipt().ListFlags(ctx, &flipt.ListFlagRequest{})
+		require.NoError(t, err)
+
+		assert.Len(t, flags.Flags, 1)
+		assert.Equal(t, updated.Key, flags.Flags[0].Key)
+		assert.Equal(t, updated.Name, flags.Flags[0].Name)
+		assert.Equal(t, updated.Description, flags.Flags[0].Description)
+
+		for _, key := range []string{"one", "two"} {
+			t.Logf("Create variant with key %q.", key)
+
+			createdVariant, err := client.Flipt().CreateVariant(ctx, &flipt.CreateVariantRequest{
+				Key:     key,
+				Name:    key,
+				FlagKey: "test",
+			})
+			require.NoError(t, err)
+
+			assert.Equal(t, key, createdVariant.Key)
+			assert.Equal(t, key, createdVariant.Name)
+		}
+
+		t.Log("Get flag \"test\" and check variants.")
+
+		flag, err = client.Flipt().GetFlag(ctx, &flipt.GetFlagRequest{Key: "test"})
+		require.NoError(t, err)
+
+		assert.Len(t, flag.Variants, 2)
+
+		t.Log("Update variant \"one\" (rename from \"one\" to \"One\")")
+
+		updatedVariant, err := client.Flipt().UpdateVariant(ctx, &flipt.UpdateVariantRequest{
+			FlagKey: "test",
+			Id:      flag.Variants[0].Id,
+			Key:     "one",
+			Name:    "One",
+		})
+		require.NoError(t, err)
+
+		assert.Equal(t, "one", updatedVariant.Key)
+		assert.Equal(t, "One", updatedVariant.Name)
 	})
-	require.NoError(t, err)
 
-	assert.Equal(t, "one", updatedVariant.Key)
-	assert.Equal(t, "One", updatedVariant.Name)
+	t.Run("Segments and Constraints", func(t *testing.T) {
+		createdSegment, err := client.Flipt().CreateSegment(ctx, &flipt.CreateSegmentRequest{
+			Key:       "everyone",
+			Name:      "Everyone",
+			MatchType: flipt.MatchType_ALL_MATCH_TYPE,
+		})
+		require.NoError(t, err)
+
+		assert.Equal(t, "everyone", createdSegment.Key)
+		assert.Equal(t, "Everyone", createdSegment.Name)
+		assert.Equal(t, flipt.MatchType_ALL_MATCH_TYPE, createdSegment.MatchType)
+	})
 }
