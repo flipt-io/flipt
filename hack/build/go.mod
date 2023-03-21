ba3e873a1aac3a68a837e22ef198b7c53b7b0701
George <me@georgemac.com>
2023-03-21 18:11:30 +0000

    fix(github): use github go client to create tags (#1425)
    * fix(github): use github go client to create tags
    
    * fix(github): pass github app credentials directly to mage task
    
    * fix(hack/build): allow tag ref to reference commit not tag

--- hack/build/go.mod
+++ hack/build/go.mod
@@ -4,8 +4,11 @@ go 1.19
 
 require (
 	dagger.io/dagger v0.5.0
+	github.com/bradleyfalzon/ghinstallation/v2 v2.2.0
 	github.com/containerd/containerd v1.6.18
 	github.com/docker/docker v23.0.1+incompatible
+	github.com/gofrs/uuid v4.4.0+incompatible
+	github.com/google/go-github/v50 v50.2.0
 	github.com/google/uuid v1.3.0
 	github.com/magefile/mage v1.14.0
 	github.com/opencontainers/image-spec v1.1.0-rc2
@@ -15,6 +18,7 @@ require (
 	golang.org/x/mod v0.9.0
 	golang.org/x/sync v0.1.0
 	google.golang.org/grpc v1.53.0
+	google.golang.org/protobuf v1.29.1
 	sigs.k8s.io/kind v0.17.0
 )
 
@@ -22,15 +26,19 @@ require (
 	github.com/BurntSushi/toml v1.2.0 // indirect
 	github.com/Khan/genqlient v0.5.0 // indirect
 	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20230217124315-7d5c6f04bbb8 // indirect
 	github.com/adrg/xdg v0.4.0 // indirect
 	github.com/alessio/shellescape v1.4.1 // indirect
+	github.com/cloudflare/circl v1.1.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/docker/distribution v2.8.1+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/safetext v0.0.0-20220905092116-b49f7bc46da2 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.2 // indirect
@@ -48,13 +56,15 @@ require (
 	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/vektah/gqlparser/v2 v2.5.1 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/oauth2 v0.6.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/text v0.8.0 // indirect
 	golang.org/x/time v0.1.0 // indirect
 	golang.org/x/tools v0.7.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
-	google.golang.org/protobuf v1.29.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- hack/build/release/release.go
+++ hack/build/release/release.go
@@ -2,35 +2,40 @@ package release
 
 import (
 	"context"
-	"errors"
 	"fmt"
+	"net/http"
 	"os"
 	"path"
+	"strconv"
 	"strings"
+	"time"
 
 	"dagger.io/dagger"
+	"github.com/bradleyfalzon/ghinstallation/v2"
+	"github.com/google/go-github/v50/github"
 	"golang.org/x/mod/semver"
 )
 
 func Submodules(ctx context.Context, client *dagger.Client, tag string) error {
-	token := os.Getenv("GITHUB_TOKEN")
-	if token == "" {
-		return errors.New("GITHUB_TOKEN is required")
+	var (
+		appID, _          = strconv.ParseInt(os.Getenv("FLIPT_RELEASE_BOT_APP_ID"), 10, 64)
+		installationID, _ = strconv.ParseInt(os.Getenv("FLIPT_RELEASE_BOT_INSTALLATION_ID"), 10, 64)
+		privateKey        = os.Getenv("FLIPT_RELEASE_BOT_APP_PEM")
+	)
+
+	itr, err := ghinstallation.New(http.DefaultTransport, appID, installationID, []byte(privateKey))
+	if err != nil {
+		return err
 	}
+
+	ghClient := github.NewClient(&http.Client{Transport: itr})
 
 	base := client.Container().From("golang:1.20-alpine3.17").
 		WithExec([]string{"apk", "add", "-U", "--no-cache", "git"}).
 		WithExec([]string{"git", "clone", "https://github.com/flipt-io/flipt.git", "/src/flipt"}).
 		WithWorkdir("/src/flipt")
 
-	authenticated := base.WithExec([]string{"git", "config", "--global", "user.name", "flipt-automation[bot]"}).
-		WithExec([]string{"git", "config", "--global", "user.email", "dev@flipt.io"}).
-		WithExec([]string{"git", "config", "--global",
-			"http.https://github.com/.extraheader",
-			fmt.Sprintf("AUTHORIZATION: Bearer %s", token),
-		})
-
-	tagList, err := authenticated.WithExec([]string{"git", "tag", "--list", "v*"}).Stdout(ctx)
+	tagList, err := base.WithExec([]string{"git", "tag", "--list", "v*"}).Stdout(ctx)
 	if err != nil {
 		return err
 	}
@@ -50,7 +55,17 @@ func Submodules(ctx context.Context, client *dagger.Client, tag string) error {
 		"errors",
 		"rpc/flipt",
 	} {
-		if err := tagSubmodule(ctx, authenticated, submodule, lastTwo[0], lastTwo[1]); err != nil {
+		diff, err := base.WithExec([]string{"git", "diff", "--shortstat", lastTwo[0], lastTwo[1], "--", submodule}).Stdout(ctx)
+		if err != nil {
+			return err
+		}
+
+		if strings.TrimSpace(diff) == "" {
+			fmt.Printf("Nothing changed between %q and %q. Skipping sub-module release.", lastTwo[0], lastTwo[1])
+			continue
+		}
+
+		if err := tagSubmodule(ctx, ghClient, submodule, lastTwo[1]); err != nil {
 			return err
 		}
 	}
@@ -58,34 +73,45 @@ func Submodules(ctx context.Context, client *dagger.Client, tag string) error {
 	return nil
 }
 
-func tagSubmodule(ctx context.Context, container *dagger.Container, submodule, fromTag, toTag string) error {
-	diff, err := container.WithExec([]string{"git", "diff", "--shortstat", fromTag, toTag, "--", submodule}).Stdout(ctx)
+func tagSubmodule(ctx context.Context, client *github.Client, submodule, tag string) error {
+	const (
+		owner = "flipt-io"
+		repo  = "flipt"
+	)
+	ref, _, err := client.Git.GetRef(ctx, owner, repo, "tags/"+tag)
 	if err != nil {
 		return err
 	}
 
-	if strings.TrimSpace(diff) == "" {
-		fmt.Printf("Nothing changed between %q and %q. Skipping sub-module release.", fromTag, toTag)
-		return nil
+	if ref.GetObject().GetType() != "tag" && ref.GetObject().GetType() != "commit" {
+		return fmt.Errorf("unexpected object type %q", ref.GetObject().GetType())
 	}
 
-	target := path.Join(submodule, toTag)
-	container = container.
-		// checkout destination version tag
-		WithExec([]string{"git", "checkout", toTag}).
-		// tag with target submodule tag
-		WithExec([]string{
-			"git", "tag", "-am",
-			fmt.Sprintf("Releasing go.flipt.io/flipt/%s version %s", submodule, toTag),
-			target,
-		})
+	target := path.Join(submodule, tag)
 
-	_, err = container.WithExec([]string{"git", "tag", "-n", target}).
-		WithExec([]string{"git", "push", "origin", target}).
-		ExitCode(ctx)
+	// create Tag object based on existing repo tag SHA
+	created, _, err := client.Git.CreateTag(ctx, owner, repo, &github.Tag{
+		Tag:     github.String(target),
+		Message: github.String(fmt.Sprintf("Releasing go.flipt.io/flipt/%s version %s", submodule, tag)),
+		Tagger: &github.CommitAuthor{
+			Name:  github.String("flipt-release[bot]"),
+			Email: github.String("dev@flipt.io"),
+			Date:  &github.Timestamp{Time: time.Now()},
+		},
+		Object: &github.GitObject{
+			Type: github.String("commit"),
+			SHA:  ref.GetObject().SHA,
+		},
+	})
 	if err != nil {
 		return err
 	}
 
-	return nil
+	// create tag ref: https://docs.github.com/en/rest/git/tags?apiVersion=2022-11-28#create-a-tag-object
+	_, _, err = client.Git.CreateRef(ctx, owner, repo, &github.Reference{
+		Ref:    github.String("tags/" + target),
+		Object: created.Object,
+	})
+
+	return err
 }
