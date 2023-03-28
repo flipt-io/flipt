1aa5750c429b2cf88b9d60e49f6c4c35634e96d3
George MacRorie <me@georgemac.com>
2023-03-28 12:17:07 +0100

    feat(hack/build): direct integration test flipt logs into directory

--- hack/build/internal/test/integration.go
+++ hack/build/internal/test/integration.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"context"
+	"fmt"
 
 	"dagger.io/dagger"
 	"github.com/google/uuid"
@@ -13,16 +14,31 @@ func Integration(ctx context.Context, client *dagger.Client, base, flipt *dagger
 		return err
 	}
 
-	integration := func(flipt *dagger.Container, flags ...string) func() error {
+	logs := client.CacheVolume(fmt.Sprintf("logs-%s", uuid.New()))
+	_, err := flipt.WithUser("root").
+		WithMountedCache("/logs", logs).
+		WithExec([]string{"chown", "flipt:flipt", "/logs"}).
+		ExitCode(ctx)
+	if err != nil {
+		return err
+	}
+
+	integration := func(name string, flipt *dagger.Container, flags ...string) func() error {
+		logFile := fmt.Sprintf("/var/opt/flipt/logs/%s.log", name)
+
+		flipt = flipt.
+			// this ensures a unique instance of flipt is created per
+			// integration test
+			WithEnvVariable("UNIQUE", uuid.New().String()).
+			WithEnvVariable("FLIPT_LOG_LEVEL", "debug").
+			WithMountedCache("/var/opt/flipt/logs", logs).
+			WithEnvVariable("FLIPT_LOG_FILE", logFile).
+			WithExec(nil)
+
 		return func() error {
 			flags := append([]string{"-v", "-race"}, flags...)
 			_, err := base.
-				WithServiceBinding("flipt", flipt.
-					// this ensures a unique instance of flipt is created per
-					// integration test
-					WithEnvVariable("UNIQUE", uuid.New().String()).
-					WithEnvVariable("FLIPT_LOG_LEVEL", "debug").
-					WithExec(nil)).
+				WithServiceBinding("flipt", flipt).
 				WithWorkdir("hack/build/integration").
 				WithExec(append([]string{"go", "test"}, append(flags, ".")...)).
 				ExitCode(ctx)
@@ -33,15 +49,24 @@ func Integration(ctx context.Context, client *dagger.Client, base, flipt *dagger
 
 	var g errgroup.Group
 
-	for _, addr := range []string{
-		"grpc://flipt:9000",
-		"http://flipt:8080",
+	for _, target := range []struct {
+		protocol string
+		address  string
+	}{
+		{"grpc", "flipt:9000"},
+		{"http", "flipt:8080"},
 	} {
-		addr := addr
-		g.Go(integration(flipt, "-flipt-addr", addr))
+		var (
+			target = target
+			addr   = fmt.Sprintf("%s://%s", target.protocol, target.address)
+			name   = fmt.Sprintf("flipt-%s", target.protocol)
+		)
+
+		g.Go(integration(name, flipt, "-flipt-addr", addr))
 
 		token := "abcdefghij"
 		g.Go(integration(
+			name+"-with-authentication",
 			flipt.
 				WithEnvVariable("FLIPT_AUTHENTICATION_REQUIRED", "true").
 				WithEnvVariable("FLIPT_AUTHENTICATION_METHODS_TOKEN_ENABLED", "true").
@@ -51,5 +76,12 @@ func Integration(ctx context.Context, client *dagger.Client, base, flipt *dagger
 		))
 	}
 
-	return g.Wait()
+	err = g.Wait()
+
+	flipt.
+		WithMountedCache("/var/opt/flipt/logs", logs).
+		WithExec([]string{"cp", "-r", "/var/opt/flipt/logs", "/var/opt/flipt/out"}).
+		Directory("/var/opt/flipt/out").Export(ctx, "hack/build/logs")
+
+	return err
 }
