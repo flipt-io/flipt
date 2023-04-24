9510d3ff13d5cd3cc2bb26a456653c3e9b46087b
George MacRorie <me@georgemac.com>
2023-04-24 17:24:41 +0100

    feat(build/ui): export playright report directory on mage test:ui

--- build/testing/ui.go
+++ build/testing/ui.go
@@ -8,14 +8,24 @@ import (
 )
 
 func UI(ctx context.Context, client *dagger.Client, ui, flipt *dagger.Container) error {
-	_, err := ui.
+	test := ui.
 		WithExec([]string{"npx", "playwright", "install", "chromium", "--with-deps"}).
 		WithServiceBinding("flipt", flipt.
 			WithEnvVariable("FLIPT_AUTHENTICATION_METHODS_TOKEN_ENABLED", "true").
 			WithEnvVariable("UNIQUE", time.Now().String()).
 			WithExec(nil)).
 		WithEnvVariable("FLIPT_ADDRESS", "http://flipt:8080").
-		WithExec([]string{"npx", "playwright", "test"}).
-		ExitCode(ctx)
-	return err
+		WithExec([]string{"npx", "playwright", "test"})
+	_, err := test.ExitCode(ctx)
+	if err != nil {
+		return err
+	}
+
+	if _, err := test.
+		Directory("playwright-report").
+		Export(ctx, "playwright-report"); err != nil {
+		return err
+	}
+
+	return nil
 }
