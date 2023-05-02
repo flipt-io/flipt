518d6ed2a8924bd009ad34d61e5d3f1c2610a2da
Yoofi Quansah <ybquansah@gmail.com>
2023-05-02 09:14:00 -0500

    chore: validate cue against yaml files coming in from git repo

--- cue/validate.go
+++ cue/validate.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"os"
 
+	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/cuecontext"
 	"cuelang.org/go/pkg/encoding/yaml"
 	"github.com/hashicorp/go-multierror"
@@ -13,6 +14,27 @@ var (
 	//go:embed flipt.cue
 	cueFile []byte
 )
+
+// ValidateBytes takes a slice of bytes, and validates them against a cue definition.
+func ValidateBytes(b []byte) error {
+	ctx := cuecontext.New()
+
+	v := ctx.CompileBytes(cueFile)
+	if err := v.Err(); err != nil {
+		return err
+	}
+
+	return validateHelper(b, v)
+}
+
+func validateHelper(b []byte, v cue.Value) error {
+	_, err := yaml.Validate(b, v)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Validate takes a slice of strings as filenames and validates them against
 // our cue definition of features.
@@ -33,7 +55,7 @@ func Validate(files []string) error {
 		if err != nil {
 			return err
 		}
-		_, err = yaml.Validate(b, v)
+		err = validateHelper(b, v)
 		if err != nil {
 			merr = multierror.Append(merr, err)
 		}
