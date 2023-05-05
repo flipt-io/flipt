98a845d96d4e2630dccace59c3e9c3e1f6d6e492
George MacRorie <me@georgemac.com>
2023-05-05 11:21:46 +0100

    chore(storage/sql): restore old limit offset pagination test

--- internal/storage/sql/flag_test.go
+++ internal/storage/sql/flag_test.go
@@ -2,13 +2,14 @@ package sql_test
 
 import (
 	"context"
-	"crypto/rand"
+	"encoding/json"
 	"fmt"
 	"testing"
 	"time"
 
 	"go.flipt.io/flipt/internal/storage"
 	fliptsql "go.flipt.io/flipt/internal/storage/sql"
+	"go.flipt.io/flipt/internal/storage/sql/common"
 	flipt "go.flipt.io/flipt/rpc/flipt"
 
 	"github.com/gofrs/uuid"
@@ -233,63 +234,93 @@ func (s *DBTestSuite) TestListFlagsPagination_LimitOffset() {
 func (s *DBTestSuite) TestListFlagsPagination_LimitWithNextPage() {
 	t := s.T()
 
-	namespace := uuid.Must(uuid.NewV4()).String()
+	reqs := []*flipt.CreateFlagRequest{
+		{
+			Key:         uuid.Must(uuid.NewV4()).String(),
+			Name:        "foo",
+			Description: "bar",
+			Enabled:     true,
+		},
+		{
+			Key:         uuid.Must(uuid.NewV4()).String(),
+			Name:        "foo",
+			Description: "bar",
+		},
+		{
+			Key:         uuid.Must(uuid.NewV4()).String(),
+			Name:        "foo",
+			Description: "bar",
+			Enabled:     true,
+		},
+	}
 
-	ctx := context.Background()
-	_, err := s.store.CreateNamespace(ctx, &flipt.CreateNamespaceRequest{
-		Key: namespace,
-	})
-	require.NoError(t, err)
-
-	var (
-		totalFlags = 103
-		pageSize   = uint64(13)
-	)
-
-	for i := 0; i < totalFlags; i++ {
-		req := flipt.CreateFlagRequest{
-			NamespaceKey: namespace,
-			Key:          fmt.Sprintf("flag_%03d", i),
-			Name:         "foo",
-			Description:  "bar",
-		}
-
+	for _, req := range reqs {
 		if s.db.Driver == fliptsql.MySQL {
 			// required for MySQL since it only s.stores timestamps to the second and not millisecond granularity
 			time.Sleep(time.Second)
 		}
-
-		_, err := s.store.CreateFlag(ctx, &req)
+		_, err := s.store.CreateFlag(context.TODO(), req)
 		require.NoError(t, err)
-
-		for i := 0; i < rand.Intn(5); i++ {
-			_, err := s.store.CreateVariant(ctx, &flipt.CreateVariantRequest{
-				NamespaceKey: namespace,
-				FlagKey:      req.Key,
-				Key:          fmt.Sprintf("variant_%d", i),
-			})
-			require.NoError(t, err)
-		}
 	}
 
-	resp, err := s.store.ListFlags(ctx, namespace,
-		storage.WithLimit(pageSize))
+	oldest, middle, newest := reqs[0], reqs[1], reqs[2]
+
+	// TODO: the ordering (DESC) is required because the default ordering is ASC and we are not clearing the DB between tests
+	// get newest flag
+	opts := []storage.QueryOption{storage.WithOrder(storage.OrderDesc), storage.WithLimit(1)}
+
+	res, err := s.store.ListFlags(context.TODO(), storage.DefaultNamespace, opts...)
 	require.NoError(t, err)
 
-	found := resp.Results
-	for token := resp.NextPageToken; token != ""; token = resp.NextPageToken {
-		resp, err = s.store.ListFlags(ctx, namespace,
-			storage.WithLimit(pageSize),
-			storage.WithPageToken(token),
-		)
-		require.NoError(t, err)
+	got := res.Results
+	assert.Len(t, got, 1)
+	assert.Equal(t, newest.Key, got[0].Key)
+	assert.NotEmpty(t, res.NextPageToken)
 
-		found = append(found, resp.Results...)
-	}
+	pageToken := &common.PageToken{}
+	err = json.Unmarshal([]byte(res.NextPageToken), pageToken)
+	require.NoError(t, err)
+	// next page should be the middle flag
+	assert.Equal(t, middle.Key, pageToken.Key)
+	assert.NotZero(t, pageToken.Offset)
 
-	assert.Len(t, found, totalFlags)
+	opts = append(opts, storage.WithPageToken(res.NextPageToken))
+
+	// get middle flag
+	res, err = s.store.ListFlags(context.TODO(), storage.DefaultNamespace, opts...)
+	require.NoError(t, err)
+
+	got = res.Results
+	assert.Len(t, got, 1)
+	assert.Equal(t, middle.Key, got[0].Key)
+
+	err = json.Unmarshal([]byte(res.NextPageToken), pageToken)
+	require.NoError(t, err)
+	// next page should be the oldest flag
+	assert.Equal(t, oldest.Key, pageToken.Key)
+	assert.NotZero(t, pageToken.Offset)
+
+	opts = []storage.QueryOption{storage.WithOrder(storage.OrderDesc), storage.WithLimit(1), storage.WithPageToken(res.NextPageToken)}
+
+	// get oldest flag
+	res, err = s.store.ListFlags(context.TODO(), storage.DefaultNamespace, opts...)
+	require.NoError(t, err)
+
+	got = res.Results
+	assert.Len(t, got, 1)
+	assert.Equal(t, oldest.Key, got[0].Key)
+
+	opts = []storage.QueryOption{storage.WithOrder(storage.OrderDesc), storage.WithLimit(3)}
+	// get all flags
+	res, err = s.store.ListFlags(context.TODO(), storage.DefaultNamespace, opts...)
+	require.NoError(t, err)
+
+	got = res.Results
+	assert.Len(t, got, 3)
+	assert.Equal(t, newest.Key, got[0].Key)
+	assert.Equal(t, middle.Key, got[1].Key)
+	assert.Equal(t, oldest.Key, got[2].Key)
 }
-
 func (s *DBTestSuite) TestListFlagsPagination_FullWalk() {
 	t := s.T()
 
