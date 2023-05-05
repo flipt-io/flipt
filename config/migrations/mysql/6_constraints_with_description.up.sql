cfd70cb6c40193f5fe30a3f964fa30bdd3f20ac8
Yoofi Quansah <ybquansah@gmail.com>
2023-05-04 20:04:17 -0500

    chore: change migrations to accept default value

--- config/migrations/mysql/6_constraints_with_description.up.sql
+++ config/migrations/mysql/6_constraints_with_description.up.sql
@@ -1,2 +1,2 @@
 -- Add description column to constraints
-ALTER TABLE constraints ADD COLUMN description TEXT;
+ALTER TABLE constraints ADD COLUMN description TEXT NOT NULL DEFAULT 'desc';
--- config/migrations/sqlite3/8_constraints_with_description.up.sql
+++ config/migrations/sqlite3/8_constraints_with_description.up.sql
@@ -1,2 +1,4 @@
+PRAGMA foreign_keys = 0;
 -- Add description column to constraints
-ALTER TABLE constraints ADD COLUMN description TEXT;
+ALTER TABLE constraints ADD COLUMN description TEXT NOT NULL DEFAULT 'desc';
+PRAGMA foriegn_keys = 1;
