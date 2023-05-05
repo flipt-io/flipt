42da6b997ca1e22ef2bce6f4d3b4cb989eab2933
Yoofi Quansah <ybquansah@gmail.com>
2023-05-05 08:30:15 -0500

    chore: try paranthesis around default value

--- config/migrations/mysql/6_constraints_with_description.up.sql
+++ config/migrations/mysql/6_constraints_with_description.up.sql
@@ -1,2 +1,2 @@
 -- Add description column to constraints
-ALTER TABLE constraints ADD COLUMN description TEXT NOT NULL DEFAULT 'desc';
+ALTER TABLE constraints ADD COLUMN description TEXT NOT NULL DEFAULT ('desc');
