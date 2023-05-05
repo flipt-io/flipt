9c16eb6ceb0ab3bf9dcb8df8641c06044d41d873
Yoofi Quansah <ybquansah@gmail.com>
2023-05-05 10:47:39 -0500

    chore: make description nullable

--- config/migrations/sqlite3/8_constraints_with_description.up.sql
+++ config/migrations/sqlite3/8_constraints_with_description.up.sql
@@ -1,4 +1,2 @@
-PRAGMA foreign_keys = 0;
 -- Add description column to constraints
-ALTER TABLE constraints ADD COLUMN description TEXT NOT NULL DEFAULT 'desc';
-PRAGMA foriegn_keys = 1;
+ALTER TABLE constraints ADD COLUMN description TEXT;
