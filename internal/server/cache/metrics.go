97d1d43023b28874b9992300e3c31a12f5eb2b84
George MacRorie <me@georgemac.com>
2023-05-17 14:06:43 +0100

    fix(metrics): update callsites to conform with otel metrics packages

--- internal/server/cache/metrics.go
+++ internal/server/cache/metrics.go
@@ -6,6 +6,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"go.flipt.io/flipt/internal/metrics"
 	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/metric/instrument"
 )
 
@@ -19,24 +20,26 @@ var (
 	Hit = metrics.MustInt64().
 		Counter(
 			prometheus.BuildFQName(namespace, subsystem, "hit"),
-			instrument.WithDescription("The number of cache hits"),
+			metric.WithDescription("The number of cache hits"),
 		)
 		// Miss is a counter for cache misses.
 	Miss = metrics.MustInt64().
 		Counter(
 			prometheus.BuildFQName(namespace, subsystem, "miss"),
-			instrument.WithDescription("The number of cache misses"),
+			metric.WithDescription("The number of cache misses"),
 		)
 		// Error is a counter for cache errors.
 	Error = metrics.MustInt64().
 		Counter(
 			prometheus.BuildFQName(namespace, subsystem, "error"),
-			instrument.WithDescription("The number of times an error occurred reading or writing to the cache"),
+			metric.WithDescription("The number of times an error occurred reading or writing to the cache"),
 		)
 )
 
 // Observe adds one to the provided counter and records the
 // cache type attribute supplied by typ.
 func Observe(ctx context.Context, typ string, counter instrument.Int64Counter) {
-	counter.Add(ctx, 1, attribute.Key("cache").String(typ))
+	counter.Add(ctx, 1, metric.WithAttributeSet(
+		attribute.NewSet(attribute.Key("cache").String(typ)),
+	))
 }
