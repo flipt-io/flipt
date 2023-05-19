76a34879492c6f92e0a2a46dc9d8b54b92382f0c
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-05-19 08:05:07 -0400

    fix: set CI in IT containers; try to use GH reporter in CI

--- build/testing/ui.go
+++ build/testing/ui.go
@@ -2,6 +2,7 @@ package testing
 
 import (
 	"context"
+	"os"
 	"time"
 
 	"dagger.io/dagger"
@@ -11,11 +12,12 @@ func UI(ctx context.Context, client *dagger.Client, ui, flipt *dagger.Container)
 	test := ui.
 		WithExec([]string{"npx", "playwright", "install", "chromium", "--with-deps"}).
 		WithServiceBinding("flipt", flipt.
+			WithEnvVariable("CI", os.Getenv("CI")).
 			WithEnvVariable("FLIPT_AUTHENTICATION_METHODS_TOKEN_ENABLED", "true").
 			WithEnvVariable("UNIQUE", time.Now().String()).
 			WithExec(nil)).
 		WithEnvVariable("FLIPT_ADDRESS", "http://flipt:8080").
-		WithExec([]string{"npx", "playwright", "test", "--reporter=dot"})
+		WithExec([]string{"npx", "playwright", "test"})
 	_, err := test.ExitCode(ctx)
 	if err != nil {
 		return err
