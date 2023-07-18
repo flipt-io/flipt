07459a06649ac3ec5bb59ab1771112d0f7ab9153
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-07-18 12:59:48 -0400

    chore(wip): ui it tests

---
+++ ui/tests/rollouts.spec.ts
@@ -0,0 +1,71 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('Rollouts', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+    await page.getByRole('link', { name: 'Flags' }).click();
+  });
+
+  test('can create boolean flag', async ({ page }) => {
+    await page.getByRole('button', { name: 'New Flag' }).click();
+    await page.getByLabel('Name').fill('test-boolean');
+    await page.getByLabel('Boolean').check();
+    await page.getByRole('button', { name: 'Create' }).click();
+    await expect(page.getByText('Successfully created flag')).toBeVisible();
+    await page.getByText('Evaluation').click();
+    await expect(
+      page.getByRole('heading', { name: 'Default Rollout' })
+    ).toBeVisible();
+  });
+
+  test('can create rollout', async ({ page }) => {
+    await page.getByRole('link', { name: 'test-boolean' }).click();
+    await page.getByRole('button', { name: 'New Rollout' }).click();
+    await page.getByLabel('Percentage').fill('100');
+    await page.getByLabel('Value').selectOption('false');
+    await page.getByRole('textbox').fill('test'); // TODO: should get description by label
+    await page.getByRole('button', { name: 'Create' }).click();
+    await expect(page.getByText('Successfully created rollout')).toBeVisible();
+    await page.getByLabel('Percentage').fill('70');
+    await page.getByLabel('Percentage').click();
+    await page.getByRole('button', { name: 'Reset' }).first().click();
+    await expect(page.getByLabel('Percentage')).toHaveValue('100');
+  });
+
+  test('can quick edit rollout', async ({ page }) => {
+    await page.getByRole('link', { name: 'test-boolean' }).click();
+    await page.getByRole('list').getByLabel('Percentage').first().fill('70');
+    await page
+      .getByRole('list')
+      .getByRole('button', { name: 'Update' })
+      .first()
+      .click();
+    await expect(page.getByText('Successfully updated rollout')).toBeVisible();
+  });
+
+  test('can edit rollout', async ({ page }) => {
+    await page.getByRole('link', { name: 'test-boolean' }).click();
+    await page
+      .getByRole('list')
+      .locator('[id="headlessui-menu-button-\\:r4\\:"]')
+      .first()
+      .click();
+    await page.getByRole('menuitem', { name: 'Edit' }).click();
+    await page.getByRole('textbox').click();
+    await page.getByRole('textbox').fill('test2');
+    await page.getByRole('button', { name: 'Update' }).click();
+    await expect(page.getByText('Successfully updated rollout')).toBeVisible();
+  });
+
+  test('can delete rollout', async ({ page }) => {
+    await page.getByRole('link', { name: 'test-boolean' }).click();
+    await page
+      .getByRole('list')
+      .locator('[id="headlessui-menu-button-\\:r4\\:"]')
+      .first()
+      .click();
+    await page.getByRole('menuitem', { name: 'Delete' }).click();
+    await page.getByRole('button', { name: 'Delete' }).click();
+    await expect(page.getByText('Successfully deleted rollout')).toBeVisible();
+  });
+});
