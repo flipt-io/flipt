46b845feb9423242e9332c65e1b079c26ecbcab5
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-07-25 07:52:07 -0400

    chore: rm old screenshots func

--- build/testing/ui.go
+++ build/testing/ui.go
@@ -2,14 +2,10 @@ package testing
 
 import (
 	"context"
-	"crypto/sha256"
-	"fmt"
 	"os"
-	"path"
 	"time"
 
 	"dagger.io/dagger"
-	"golang.org/x/sync/errgroup"
 )
 
 func UI(ctx context.Context, client *dagger.Client, ui, flipt *dagger.Container) error {
@@ -30,97 +26,6 @@ func UI(ctx context.Context, client *dagger.Client, ui, flipt *dagger.Container)
 		Export(ctx, "playwright-report")
 
 	return nil
-}
-
-func Screenshots(ctx context.Context, client *dagger.Client, flipt *dagger.Container) error {
-	src := client.Host().Directory("./ui/", dagger.HostDirectoryOpts{
-		Include: []string{
-			"./package.json",
-			"./package-lock.json",
-			"./playwright.config.ts",
-			"/screenshots/",
-		},
-	})
-
-	contents, err := src.File("package-lock.json").Contents(ctx)
-	if err != nil {
-		return err
-	}
-
-	cache := client.CacheVolume(fmt.Sprintf("node-modules-screenshot-%x", sha256.Sum256([]byte(contents))))
-
-	ui, err := client.Container().From("node:18-bullseye").
-		WithMountedDirectory("/src", src).WithWorkdir("/src").
-		WithMountedCache("/src/node_modules", cache).
-		WithExec([]string{"npm", "install"}).
-		WithExec([]string{"npx", "playwright", "install", "chromium", "--with-deps"}).
-		Sync(ctx)
-	if err != nil {
-		return err
-	}
-
-	src = client.Host().Directory("./ui/", dagger.HostDirectoryOpts{
-		Exclude: []string{
-			"./dist/",
-			"./node_modules/",
-		},
-	})
-
-	// remount entire directory with module cache
-	ui, err = ui.WithMountedDirectory("/src", src).
-		WithMountedCache("/src/node_modules", cache).
-		WithExec([]string{"npm", "install"}).
-		Sync(ctx)
-	if err != nil {
-		return err
-	}
-
-	entries, err := ui.Directory("screenshot").Entries(ctx)
-	if err != nil {
-		return err
-	}
-
-	var (
-		g          errgroup.Group
-		containers = make(chan *dagger.Container)
-	)
-
-	go func() {
-		_ = g.Wait()
-		close(containers)
-	}()
-
-	for _, entry := range entries {
-		entry := entry
-		g.Go(func() error {
-			test, err := buildUI(ctx, ui, flipt)
-			if err != nil {
-				return err
-			}
-
-			if ext := path.Ext(entry); ext != ".js" {
-				return nil
-			}
-
-			c, err := test.WithExec([]string{"node", path.Join("screenshot", entry)}).Sync(ctx)
-			if err != nil {
-				return err
-			}
-
-			containers <- c
-
-			return err
-		})
-	}
-
-	for c := range containers {
-		if _, err := c.Directory("screenshots").
-			Export(ctx, "screenshots"); err != nil {
-			return err
-		}
-	}
-
-	return g.Wait()
 }
 
 func buildUI(ctx context.Context, ui, flipt *dagger.Container) (_ *dagger.Container, err error) {
