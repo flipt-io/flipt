d3c0cb46477e32108291c03ffc15f0dbb84cef9b
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-07-28 14:06:41 -0400

    chore: disallow rollouts eval UI (#1917)
    * chore: disallow navigation to /evaluation in UI for boolean flags
    
    * chore: fix error message if not error type
    
    * chore: fix null error
    
    * chore: fix tests

--- ui/src/utils/helpers.ts
+++ ui/src/utils/helpers.ts
@@ -36,3 +36,32 @@ export function addNamespaceToPath(path: string, key: string): string {
   }
   return `${namespaces}${key}${path}`;
 }
+
+type ErrorWithMessage = {
+  message: string;
+};
+
+function isErrorWithMessage(error: unknown): error is ErrorWithMessage {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'message' in error &&
+    typeof (error as Record<string, unknown>).message === 'string'
+  );
+}
+
+function toErrorWithMessage(maybeError: unknown): ErrorWithMessage {
+  if (isErrorWithMessage(maybeError)) return maybeError;
+
+  try {
+    return new Error(JSON.stringify(maybeError));
+  } catch {
+    // fallback in case there's an error stringifying the maybeError
+    // like with circular references for example.
+    return new Error(String(maybeError));
+  }
+}
+
+export function getErrorMessage(error: unknown) {
+  return toErrorWithMessage(error).message;
+}
