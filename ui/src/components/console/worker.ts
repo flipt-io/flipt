5865c6c703baeabf692d1ca1bfb67b20aa00e1d4
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-08-24 09:05:26 -0400

    chore: add back default editor worker

--- ui/src/components/console/worker.ts
+++ ui/src/components/console/worker.ts
@@ -1,12 +1,21 @@
 // @ts-ignore
 self.MonacoEnvironment = {
-  getWorker(_moduleId, _label) {
-    const worker = new Worker(
-      new URL(
-        'monaco-editor/esm/vs/language/json/json.worker.js',
-        import.meta.url
-      )
-    );
-    return worker;
+  getWorker(_, label) {
+    switch (label) {
+      case 'json':
+        return new Worker(
+          new URL(
+            'monaco-editor/esm/vs/language/json/json.worker.js',
+            import.meta.url
+          )
+        );
+      default:
+        return new Worker(
+          new URL(
+            'monaco-editor/esm/vs/editor/editor.worker.js',
+            import.meta.url
+          )
+        );
+    }
   }
 };
