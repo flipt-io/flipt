4eb009d2c63294c587f874a7c9961aacb9723e44
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-08-30 15:23:58 -0400

    feat(hackweek): add support page instead of link to docs

--- ui/src/components/Nav.tsx
+++ ui/src/components/Nav.tsx
@@ -1,8 +1,8 @@
 import {
-  BookOpenIcon,
   CodeBracketIcon,
   Cog6ToothIcon,
   FlagIcon,
+  QuestionMarkCircleIcon,
   UsersIcon
 } from '@heroicons/react/24/outline';
 import { useSelector } from 'react-redux';
@@ -19,28 +19,13 @@ type NavItemProps = {
   to: string;
   name: string;
   Icon: Icon;
-  external?: boolean;
   onClick?: () => void;
 };
 
 function NavItem(props: NavItemProps) {
-  const { to, name, Icon, external, onClick } = props;
+  const { to, name, Icon, onClick } = props;
 
-  return external ? (
-    <a
-      key={name}
-      href={to}
-      target="_blank"
-      rel="noreferrer"
-      className="group text-white flex items-center rounded-md px-2 py-2 text-sm font-medium hover:bg-violet-400 md:text-gray-600 md:hover:bg-gray-50"
-    >
-      <Icon
-        className="text-wite mr-3 h-6 w-6 flex-shrink-0 hover:bg-gray-50 md:text-gray-500"
-        aria-hidden="true"
-      />
-      {name}
-    </a>
-  ) : (
+  return (
     <NavLink
       key={name}
       to={to}
@@ -114,10 +99,9 @@ export default function Nav(props: NavProps) {
       Icon: Cog6ToothIcon
     },
     {
-      name: 'Documentation',
-      to: 'https://flipt.io/docs?utm_source=app',
-      Icon: BookOpenIcon,
-      external: true
+      name: 'Support',
+      to: 'support',
+      Icon: QuestionMarkCircleIcon
     }
   ];
 
