8bfa792d43757c679c15afd05853e0e1a19ede83
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-09-21 19:39:59 -0400

    feat: add force flag to overwrite existing config

--- cmd/flipt/config.go
+++ cmd/flipt/config.go
@@ -10,62 +10,75 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+type initCommand struct {
+	file  string
+	force bool
+}
+
+func (c *initCommand) run(cmd *cobra.Command, args []string) error {
+	cfg := config.Default()
+	cfg.Version = config.Version // write version for backward compatibility
+	out, err := yaml.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	var b bytes.Buffer
+	b.WriteString("# yaml-language-server: $schema=https://raw.githubusercontent.com/flipt-io/flipt/main/config/flipt.schema.json\n\n")
+	b.Write(out)
+
+	answer := struct {
+		File string
+	}{}
+
+	q := []*survey.Question{
+		{
+			Name: "file",
+			Prompt: &survey.Input{
+				Message: "Configuration file path:",
+				Default: fliptConfigFile,
+			},
+			Validate: survey.Required,
+		},
+	}
+
+	if err := survey.Ask(q, &answer); err != nil {
+		return err
+	}
+
+	overwrite := c.force
+	if !overwrite {
+		// check if file exists
+		if _, err := os.Stat(answer.File); err == nil {
+			// file exists
+			prompt := &survey.Confirm{
+				Message: "File exists. Overwrite?",
+			}
+			if err := survey.AskOne(prompt, &overwrite); err != nil {
+				return err
+			}
+		}
+	}
+
+	return os.WriteFile(answer.File, b.Bytes(), 0600)
+}
+
 func newConfigCommand() *cobra.Command {
 	var configCmd = &cobra.Command{
 		Use:   "config",
 		Short: "Manage Flipt configuration",
 	}
 
-	var initCmd = &cobra.Command{
+	initCmd := &initCommand{}
+
+	var init = &cobra.Command{
 		Use:   "init",
 		Short: "Initialize Flipt configuration",
-		RunE: func(_ *cobra.Command, _ []string) error {
-			cfg := config.Default()
-			cfg.Version = config.Version // write version for backward compatibility
-			out, err := yaml.Marshal(cfg)
-			if err != nil {
-				return err
-			}
-
-			var b bytes.Buffer
-			b.WriteString("# yaml-language-server: $schema=https://raw.githubusercontent.com/flipt-io/flipt/main/config/flipt.schema.json\n\n")
-			b.Write(out)
-
-			answer := struct {
-				File string
-			}{}
-
-			q := []*survey.Question{
-				{
-					Name: "file",
-					Prompt: &survey.Input{
-						Message: "Configuration file path:",
-						Default: fliptConfigFile,
-					},
-					Validate: survey.Required,
-				},
-			}
-
-			if err := survey.Ask(q, &answer); err != nil {
-				return err
-			}
-
-			// check if file exists
-			if _, err := os.Stat(answer.File); err == nil {
-				// file exists
-				overwrite := false
-				prompt := &survey.Confirm{
-					Message: "File exists. Overwrite?",
-				}
-				if err := survey.AskOne(prompt, &overwrite); err != nil {
-					return err
-				}
-			}
-
-			return os.WriteFile(answer.File, b.Bytes(), 0600)
-		},
+		RunE:  initCmd.run,
 	}
 
-	configCmd.AddCommand(initCmd)
+	init.Flags().BoolVarP(&initCmd.force, "force", "y", false, "Overwrite existing configuration file")
+
+	configCmd.AddCommand(init)
 	return configCmd
 }
