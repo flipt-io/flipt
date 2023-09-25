eb30de9b081e8faed50927f2e6836e56472b3432
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-09-25 11:54:15 -0400

    feat(wip): config edit command

--- cmd/flipt/config.go
+++ cmd/flipt/config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 
 	"github.com/AlecAivazis/survey/v2"
@@ -67,13 +68,56 @@ func (c *initCommand) run(cmd *cobra.Command, args []string) error {
 	return os.WriteFile(file, b.Bytes(), 0600)
 }
 
+type editCommand struct{}
+
+func (c *editCommand) run(cmd *cobra.Command, args []string) error {
+	file := providedConfigFile
+
+	if file == "" {
+		file = userConfigFile
+	}
+
+	b, err := os.ReadFile(file)
+	if err != nil {
+		return fmt.Errorf("reading config file: %w", err)
+	}
+
+	content := string(b)
+
+	if err := survey.AskOne(&survey.Editor{
+		Message:       "Edit configuration",
+		Default:       string(b),
+		HideDefault:   true,
+		AppendDefault: true,
+	}, &content); err != nil {
+		return err
+	}
+
+	// create if not exists, truncate if exists
+	f, err := os.Create(file)
+	if err != nil {
+		return fmt.Errorf("creating config file: %w", err)
+	}
+
+	defer f.Close()
+
+	if _, err := f.WriteString(content); err != nil {
+		return fmt.Errorf("writing config file: %w", err)
+	}
+
+	return nil
+}
+
 func newConfigCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "config",
 		Short: "Manage Flipt configuration",
 	}
 
-	initCmd := &initCommand{}
+	var (
+		initCmd = &initCommand{}
+		editCmd = &editCommand{}
+	)
 
 	var init = &cobra.Command{
 		Use:   "init",
@@ -83,8 +127,15 @@ func newConfigCommand() *cobra.Command {
 
 	init.Flags().BoolVarP(&initCmd.force, "force", "y", false, "Overwrite existing configuration file")
 
+	var edit = &cobra.Command{
+		Use:   "edit",
+		Short: "Edit Flipt configuration",
+		RunE:  editCmd.run,
+	}
+
 	cmd.PersistentFlags().StringVar(&providedConfigFile, "config", "", "path to config file")
 	cmd.AddCommand(init)
+	cmd.AddCommand(edit)
 
 	return cmd
 }
