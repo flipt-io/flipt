654968e050a355db3570fec6dca72009b406f53c
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-09-26 17:11:00 -0400

    feat: config edit command (#2169)
    * feat(wip): cmd shell completions
    
    * feat(wip): config edit command
    
    * fix: bugs in config init
    
    * chore: setup completions with goreleaser/brew
    
    * chore: update tmp file; add no-tty comment
    
    * chore: add cfg alias

--- cmd/flipt/config.go
+++ cmd/flipt/config.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/spf13/cobra"
@@ -15,42 +17,52 @@ type initCommand struct {
 }
 
 func (c *initCommand) run(cmd *cobra.Command, args []string) error {
-	var (
-		file        string
-		defaultFile = providedConfigFile
-	)
+	defaultFile := providedConfigFile
 
 	if defaultFile == "" {
 		defaultFile = userConfigFile
 	}
 
-	q := []*survey.Question{
-		{
-			Name: "file",
-			Prompt: &survey.Input{
-				Message: "Configuration file path:",
-				Default: defaultFile,
+	file := defaultFile
+
+	ack := c.force
+	if !ack {
+		q := []*survey.Question{
+			{
+				Name: "file",
+				Prompt: &survey.Input{
+					Message: "Configuration file path:",
+					Default: defaultFile,
+				},
+				Validate: survey.Required,
 			},
-			Validate: survey.Required,
-		},
-	}
+		}
 
-	if err := survey.Ask(q, &file); err != nil {
-		return err
-	}
+		if err := survey.Ask(q, &file); err != nil {
+			return err
+		}
 
-	overwrite := c.force
-	if !overwrite {
 		// check if file exists
 		if _, err := os.Stat(file); err == nil {
 			// file exists
 			prompt := &survey.Confirm{
 				Message: "File exists. Overwrite?",
 			}
-			if err := survey.AskOne(prompt, &overwrite); err != nil {
+			if err := survey.AskOne(prompt, &ack); err != nil {
 				return err
 			}
+		} else if !os.IsNotExist(err) {
+			return err
 		}
+	}
+
+	if !ack {
+		return nil
+	}
+
+	// get path to file, create directory if not exists
+	if err := os.MkdirAll(filepath.Dir(file), 0700); err != nil {
+		return err
 	}
 
 	cfg := config.Default()
@@ -64,16 +76,67 @@ func (c *initCommand) run(cmd *cobra.Command, args []string) error {
 	b.WriteString("# yaml-language-server: $schema=https://raw.githubusercontent.com/flipt-io/flipt/main/config/flipt.schema.json\n\n")
 	b.Write(out)
 
-	return os.WriteFile(file, b.Bytes(), 0600)
+	if err := os.WriteFile(file, b.Bytes(), 0600); err != nil {
+		return err
+	}
+
+	fmt.Printf("Configuration file written to %s\n", file)
+	return nil
+}
+
+type editCommand struct{}
+
+func (c *editCommand) run(cmd *cobra.Command, args []string) error {
+	// TODO: check if no TTY
+	file := providedConfigFile
+
+	if file == "" {
+		file = userConfigFile
+	}
+
+	b, err := os.ReadFile(file)
+	if err != nil {
+		return fmt.Errorf("reading config file: %w", err)
+	}
+
+	content := string(b)
+
+	if err := survey.AskOne(&survey.Editor{
+		Message:       "Edit configuration",
+		Default:       string(b),
+		HideDefault:   true,
+		AppendDefault: true,
+		FileName:      "flipt*.yml",
+	}, &content); err != nil {
+		return err
+	}
+
+	// create if not exists, truncate if exists
+	f, err := os.Create(file)
+	if err != nil {
+		return fmt.Errorf("creating config file: %w", err)
+	}
+
+	defer f.Close()
+
+	if _, err := f.WriteString(content); err != nil {
+		return fmt.Errorf("writing config file: %w", err)
+	}
+
+	return nil
 }
 
 func newConfigCommand() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "config",
-		Short: "Manage Flipt configuration",
+		Use:     "config",
+		Short:   "Manage Flipt configuration",
+		Aliases: []string{"cfg"},
 	}
 
-	initCmd := &initCommand{}
+	var (
+		initCmd = &initCommand{}
+		editCmd = &editCommand{}
+	)
 
 	var init = &cobra.Command{
 		Use:   "init",
@@ -83,8 +146,15 @@ func newConfigCommand() *cobra.Command {
 
 	init.Flags().BoolVarP(&initCmd.force, "force", "y", false, "Overwrite existing configuration file")
 
+	var edit = &cobra.Command{
+		Use:   "edit",
+		Short: "Edit Flipt configuration",
+		RunE:  editCmd.run,
+	}
+
 	cmd.PersistentFlags().StringVar(&providedConfigFile, "config", "", "path to config file")
 	cmd.AddCommand(init)
+	cmd.AddCommand(edit)
 
 	return cmd
 }
