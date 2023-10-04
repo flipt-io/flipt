b4e6e6450d1189f7f3cf77bf1d64dc739dbc3f8c
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-10-04 13:34:18 -0400

    chore: fix screenshots script; update readme some; add go sdk license (#2199)

--- ui/screenshot/getting_started/create-rule.js
+++ ui/screenshot/getting_started/create-rule.js
@@ -3,7 +3,7 @@ const { capture } = require('../../screenshot.js');
 (async () => {
   await capture('getting_started', 'create_rule', async (page) => {
     await page.getByRole('link', { name: 'new-login' }).click();
-    await page.getByRole('link', { name: 'Evaluation' }).click();
+    await page.getByRole('link', { name: 'Rules' }).click();
     await page.getByRole('button', { name: 'New Rule' }).click();
     await page.locator('#segmentKey-select-button').click();
     await page.getByText('all-users').click();
