09bdc42804e6afd2d470e883b38a66a67dcc09f0
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-10-06 07:42:27 -0400

    chore: fix remaining screenshots script issues (#2204)

--- ui/screenshot/getting_started/create-rule.js
+++ ui/screenshot/getting_started/create-rule.js
@@ -5,7 +5,7 @@ const { capture } = require('../../screenshot.js');
     await page.getByRole('link', { name: 'new-login' }).click();
     await page.getByRole('link', { name: 'Rules' }).click();
     await page.getByRole('button', { name: 'New Rule' }).click();
-    await page.locator('#segmentKey-select-button').click();
+    await page.locator('#segmentKey-0-select-button').click();
     await page.getByText('all-users').click();
     await page.getByLabel('Multi-Variate').check();
   });
