7c0e5c3a085d09f9c7f63befce6af44212d6cccb
Yoofi Quansah <ybquansah@gmail.com>
2023-11-06 14:09:40 -0600

    feat: add three basic functions for allocating engine and retrieving it

--- client/engine/src/lib.rs
+++ client/engine/src/lib.rs
@@ -1,3 +1,7 @@
+use libc::c_void;
+use snafu::{prelude::*, Whatever};
+use std::ffi::CStr;
+use std::os::raw::c_char;
 use std::sync::{Arc, Mutex};
 
 mod common;
@@ -54,4 +58,48 @@ impl Engine {
 
         boolean_eval_response.unwrap()
     }
+}
+
+/// # Safety
+///
+/// This function should not be called unless an Engine is initiated. It provides a helper
+/// utility to retrieve an Engine instance for evaluation use.
+unsafe fn get_engine<'a>(engine_ptr: *mut c_void) -> Result<&'a mut Engine, Whatever> {
+    if engine_ptr.is_null() {
+        whatever!("null pointer engine error");
+    } else {
+        Ok(unsafe { &mut *(engine_ptr as *mut Engine) })
+    }
+}
+
+/// # Safety
+///
+/// This function will initialize an Engine and return a pointer back to the caller.
+#[no_mangle]
+pub unsafe extern "C" fn initialize_engine(namespaces: *const *const c_char) -> *mut c_void {
+    let mut index = 0;
+    let mut namespaces_vec = Vec::new();
+
+    while !(*namespaces.offset(index)).is_null() {
+        let c_str = CStr::from_ptr(*namespaces.offset(index));
+        if let Ok(rust_str) = c_str.to_str() {
+            namespaces_vec.push(rust_str.to_string());
+        }
+
+        index += 1;
+    }
+
+    Box::into_raw(Box::new(Engine::new(namespaces_vec))) as *mut c_void
+}
+
+/// # Safety
+///
+/// This function will free the memory occupied by the engine.
+#[no_mangle]
+pub unsafe extern "C" fn destroy_engine(engine_ptr: *mut c_void) {
+    if engine_ptr.is_null() {
+        return;
+    }
+
+    drop(Box::from_raw(engine_ptr as *mut Engine));
 }
