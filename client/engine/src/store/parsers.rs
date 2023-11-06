6cee0f3796979bad53773af0e099bee98a80f68c
Yoofi Quansah <ybquansah@gmail.com>
2023-11-06 09:45:09 -0600

    feat: add request logic for getting document text from configurable upstream Flipt instance

--- client/engine/src/store/parsers.rs
+++ client/engine/src/store/parsers.rs
@@ -1,4 +1,5 @@
 use snafu::{prelude::*, Whatever};
+use std::env;
 use std::fs;
 use std::path::PathBuf;
 
@@ -7,23 +8,53 @@ use super::snapshot::Parser;
 
 pub struct FliptParser {
     namespaces: Vec<String>,
+    http_client: reqwest::blocking::Client,
+    http_url: String,
 }
 
 // TODO(yquansah): Implement network request here for fetching flag state from upstream
 impl Parser for FliptParser {
     fn new(namespaces: Vec<String>) -> Self {
+        // We will allow the following line to panic when an error is encountered.
+        let client = reqwest::blocking::Client::builder()
+            .timeout(std::time::Duration::from_secs(10))
+            .build()
+            .unwrap();
+
+        let http_url =
+            env::var("FLIPT_REMOTE_URL").unwrap_or(String::from("http://localhost:8080"));
+
         Self {
             namespaces: namespaces,
+            http_client: client,
+            http_url: http_url,
         }
     }
 
-    fn parse(&self, _: String) -> Result<models::Document, Whatever> {
-        Ok(models::Document {
-            version: String::from("1.0"),
-            namespace: String::from("default"),
-            flags: Vec::new(),
-            segments: Vec::new(),
-        })
+    fn parse(&self, namespace: String) -> Result<models::Document, Whatever> {
+        let response = match self
+            .http_client
+            .get(format!(
+                "{}/internal/v1/evaluation/snapshot/namespace/{}",
+                self.http_url, namespace
+            ))
+            .send()
+        {
+            Ok(resp) => resp,
+            Err(e) => whatever!("failed to make request: err {}", e),
+        };
+
+        let response_text = match response.text() {
+            Ok(t) => t,
+            Err(e) => whatever!("failed to get response body: err {}", e),
+        };
+
+        let document: models::Document = match serde_json::from_str(&response_text) {
+            Ok(doc) => doc,
+            Err(e) => whatever!("failed to deserialize text into document: err {}", e),
+        };
+
+        Ok(document)
     }
 
     fn get_namespaces(&self) -> Vec<String> {
@@ -51,7 +82,7 @@ impl Parser for TestParser {
 
         let document: models::Document = match serde_json::from_str(&state) {
             Ok(document) => document,
-            Err(e) => whatever!("error reading data from server {}", e),
+            Err(e) => whatever!("failed to deserialize text into document: err {}", e),
         };
 
         Ok(document)
