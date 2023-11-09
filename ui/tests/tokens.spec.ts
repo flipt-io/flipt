cb0f0b0ffa3365436d6315abea9d4c0ef139d436
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-11-08 19:19:29 -0500

    chore: add ui IT for namespaced token

--- ui/tests/tokens.spec.ts
+++ ui/tests/tokens.spec.ts
@@ -6,20 +6,42 @@ test.describe('API Tokens', () => {
     await page.reload();
   });
 
-  test('can create token', async ({ page }) => {
-    await page.getByRole('link', { name: 'Settings' }).click();
-    await page.getByRole('link', { name: 'API Tokens' }).click();
-    await expect(
-      page.getByRole('heading', { name: 'Static Tokens' })
-    ).toBeVisible();
-    await page.getByRole('button', { name: 'New Token' }).nth(0).click();
-    await page.getByLabel('Name').fill('abcdef');
-    await page.getByLabel('Description').fill('xyz');
-    await page.getByRole('button', { name: 'Create' }).click();
-    await page.getByRole('button', { name: 'Copy' }).click();
-    await expect(page.locator('pre')).toContainText('Copied to clipboard');
-    await page.getByRole('button', { name: 'Close', exact: true }).click();
-    await page.getByRole('cell', { name: 'abcdef', exact: true }).click();
+  test.describe('can create tokens', () => {
+    test('default token', async ({ page }) => {
+      await page.getByRole('link', { name: 'Settings' }).click();
+      await page.getByRole('link', { name: 'API Tokens' }).click();
+      await expect(
+        page.getByRole('heading', { name: 'Static Tokens' })
+      ).toBeVisible();
+      await page.getByRole('button', { name: 'New Token' }).nth(0).click();
+      await page.getByLabel('Name').fill('abcdef');
+      await page.getByLabel('Description').fill('xyz');
+      await page.getByRole('button', { name: 'Create' }).click();
+      await page.getByRole('button', { name: 'Copy' }).click();
+      await expect(page.locator('pre')).toContainText('Copied to clipboard');
+      await page.getByRole('button', { name: 'Close', exact: true }).click();
+      await page.getByRole('cell', { name: 'abcdef', exact: true }).click();
+    });
+
+    test('expiring/namespaced token', async ({ page }) => {
+      await page.getByRole('link', { name: 'Settings' }).click();
+      await page.getByRole('link', { name: 'API Tokens' }).click();
+      await expect(
+        page.getByRole('heading', { name: 'Static Tokens' })
+      ).toBeVisible();
+      await page.getByRole('button', { name: 'New Token' }).nth(0).click();
+      await page.getByLabel('Name', { exact: true }).fill('fooo');
+      await page.getByLabel('Description').press('Tab');
+      await page.getByLabel('Expires On').fill('2023-11-01');
+      await page.getByLabel('Scope this token to a single namespace').check();
+      await page.getByRole('button', { name: 'Default' }).click();
+      await page.getByLabel('Default').getByText('Default').click();
+      await page.getByRole('button', { name: 'Create' }).click();
+      await page.getByRole('button', { name: 'Copy' }).click();
+      await expect(page.locator('pre')).toContainText('Copied to clipboard');
+      await page.getByRole('button', { name: 'Close', exact: true }).click();
+      await page.getByRole('cell', { name: 'fooo', exact: true }).click();
+    });
   });
 
   test('can delete token', async ({ page }) => {
