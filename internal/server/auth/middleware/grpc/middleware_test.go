6f1ee97bcdd91d4e7c242bd12b6340dd6adcba6e
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-11-12 13:34:17 -0500

    chore: fix middleware

--- internal/server/auth/middleware/grpc/middleware_test.go
+++ internal/server/auth/middleware/grpc/middleware_test.go
@@ -24,11 +24,11 @@ type mockServer struct {
 	allowNamespacedAuth bool
 }
 
-func (s *mockServer) SkipsAuthentication() bool {
+func (s *mockServer) SkipsAuthentication(ctx context.Context) bool {
 	return s.skipsAuth
 }
 
-func (s *mockServer) AllowsNamespaceScopedAuthentication() bool {
+func (s *mockServer) AllowsNamespaceScopedAuthentication(ctx context.Context) bool {
 	return s.allowNamespacedAuth
 }
 
