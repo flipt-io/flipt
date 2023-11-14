8e0e36c5ad22e732bd40e11370a88310fdc0ed95
George MacRorie <me@georgemac.com>
2023-11-14 11:39:27 +0000

    fix(cors): support fern and customizable allowed headers

--- internal/config/cors.go
+++ internal/config/cors.go
@@ -10,12 +10,22 @@ var _ defaulter = (*CorsConfig)(nil)
 type CorsConfig struct {
 	Enabled        bool     `json:"enabled" mapstructure:"enabled" yaml:"enabled"`
 	AllowedOrigins []string `json:"allowedOrigins,omitempty" mapstructure:"allowed_origins" yaml:"allowed_origins,omitempty"`
+	AllowedHeaders []string `json:"allowed_headers,omitempty" mapstructure:"allowed_headers" yaml:"allowed_headers,omitempty"`
 }
 
 func (c *CorsConfig) setDefaults(v *viper.Viper) error {
 	v.SetDefault("cors", map[string]any{
 		"enabled":         false,
 		"allowed_origins": "*",
+		"allowed_headers": []string{
+			"Accept",
+			"Authorization",
+			"Content-Type",
+			"X-CSRF-Token",
+			"X-Fern-Language",
+			"X-Fern-SDK-Name",
+			"X-Fern-SDK-Version",
+		},
 	})
 
 	return nil
