5c7648c29b58e06ebb2f3f8df8b24bcd590ff587
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-12-10 10:32:49 -0500

    chore: pass through argos token

--- build/testing/ui.go
+++ build/testing/ui.go
@@ -42,6 +42,7 @@ func buildUI(ctx context.Context, ui, flipt *dagger.Container) (_ *dagger.Contai
 	return ui.
 		WithServiceBinding("flipt", flipt.
 			WithEnvVariable("CI", os.Getenv("CI")).
+			WithEnvVariable("ARGOS_TOKEN", os.Getenv("ARGOS_TOKEN")).
 			WithEnvVariable("FLIPT_AUTHENTICATION_METHODS_TOKEN_ENABLED", "true").
 			WithEnvVariable("UNIQUE", time.Now().String()).
 			WithExec(nil)).
