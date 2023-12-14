4970440259eac4f3d125df7b6cd8202af5c33bff
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-12-14 09:58:12 -0500

    chore(wip): support devenv

---
+++ devenv.nix
@@ -0,0 +1,23 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  # packages = [ pkgs.git ];
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = "echo hello from $GREET";
+
+  # https://devenv.sh/languages/
+  languages.go.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
