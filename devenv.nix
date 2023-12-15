f2f7e7a150fd46dcff92aac3b9e4a1c591575fcf
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-12-15 12:20:21 -0500

    chore: devenv support

--- devenv.nix
+++ devenv.nix
@@ -5,19 +5,23 @@
   # env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  # packages = [ pkgs.git ];
+  packages = [ pkgs.git pkgs.mage pkgs.gcc pkgs.sqlite pkgs.nodejs ];
 
   # https://devenv.sh/scripts/
-  # scripts.hello.exec = "echo hello from $GREET";
+  scripts.hello.exec = "echo 'hello from Flipt!'";
 
   # https://devenv.sh/languages/
   languages.go.enable = true;
+  languages.typescript.enable = true;
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
 
   # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
+  processes = {
+    backend.exec = "mage dev";
+    frontend.exec = "mage ui:dev";
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
