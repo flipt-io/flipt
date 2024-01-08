c070173b951f047b6d888baea91eca8f42065020
George MacRorie <me@georgemac.com>
2024-01-08 16:47:07 +0000

    refactor(storage/fs): add context.Context to View

--- internal/storage/fs/store.go
+++ internal/storage/fs/store.go
@@ -29,7 +29,7 @@ type ReferencedSnapshotStore interface {
 	// for the lifetime of the provided function call.
 	// It expects a reference to be supplied on calls to View.
 	// Empty reference will be regarded as the default reference by the store.
-	View(storage.Reference, func(storage.ReadOnlyStore) error) error
+	View(context.Context, storage.Reference, func(storage.ReadOnlyStore) error) error
 	fmt.Stringer
 }
 
@@ -40,7 +40,7 @@ type SnapshotStore interface {
 	// View accepts a function which takes a storage.ReadOnlyStore (*SnapshotStore).
 	// The SnapshotStore will supply a snapshot which is valid
 	// for the lifetime of the provided function call.
-	View(func(storage.ReadOnlyStore) error) error
+	View(context.Context, func(storage.ReadOnlyStore) error) error
 	fmt.Stringer
 }
 
@@ -62,12 +62,12 @@ func NewSingleReferenceStore(logger *zap.Logger, s SnapshotStore) *SingleReferen
 // View delegates to the embedded snapshot store implementation and drops the reference.
 // It logs a warning if a non-empty reference is supplied as the wrapped store
 // isn't intended for use with custom references.
-func (s *SingleReferenceSnapshotStore) View(ref storage.Reference, fn func(storage.ReadOnlyStore) error) error {
+func (s *SingleReferenceSnapshotStore) View(ctx context.Context, ref storage.Reference, fn func(storage.ReadOnlyStore) error) error {
 	if ref != "" {
 		s.logger.Warn("single reference store called with non-empty reference", zap.String("reference", string(ref)))
 	}
 
-	return s.SnapshotStore.View(fn)
+	return s.SnapshotStore.View(ctx, fn)
 }
 
 // Store embeds a StoreSnapshot and wraps the Store methods with a read-write mutex
@@ -85,126 +85,126 @@ func (s *Store) String() string {
 }
 
 func (s *Store) GetFlag(ctx context.Context, req storage.ResourceRequest) (flag *flipt.Flag, err error) {
-	return flag, s.viewer.View(req.Reference, func(ss storage.ReadOnlyStore) error {
+	return flag, s.viewer.View(ctx, req.Reference, func(ss storage.ReadOnlyStore) error {
 		flag, err = ss.GetFlag(ctx, req)
 		return err
 	})
 }
 
 func (s *Store) ListFlags(ctx context.Context, req *storage.ListRequest[storage.NamespaceRequest]) (set storage.ResultSet[*flipt.Flag], err error) {
-	return set, s.viewer.View(req.Predicate.Reference, func(ss storage.ReadOnlyStore) error {
+	return set, s.viewer.View(ctx, req.Predicate.Reference, func(ss storage.ReadOnlyStore) error {
 		set, err = ss.ListFlags(ctx, req)
 		return err
 	})
 }
 
 func (s *Store) CountFlags(ctx context.Context, p storage.NamespaceRequest) (count uint64, err error) {
-	return count, s.viewer.View(p.Reference, func(ss storage.ReadOnlyStore) error {
+	return count, s.viewer.View(ctx, p.Reference, func(ss storage.ReadOnlyStore) error {
 		count, err = ss.CountFlags(ctx, p)
 		return err
 	})
 }
 
 func (s *Store) GetRule(ctx context.Context, p storage.NamespaceRequest, id string) (rule *flipt.Rule, err error) {
-	return rule, s.viewer.View(p.Reference, func(ss storage.ReadOnlyStore) error {
+	return rule, s.viewer.View(ctx, p.Reference, func(ss storage.ReadOnlyStore) error {
 		rule, err = ss.GetRule(ctx, p, id)
 		return err
 	})
 }
 
 func (s *Store) ListRules(ctx context.Context, req *storage.ListRequest[storage.ResourceRequest]) (set storage.ResultSet[*flipt.Rule], err error) {
-	return set, s.viewer.View(req.Predicate.Reference, func(ss storage.ReadOnlyStore) error {
+	return set, s.viewer.View(ctx, req.Predicate.Reference, func(ss storage.ReadOnlyStore) error {
 		set, err = ss.ListRules(ctx, req)
 		return err
 	})
 }
 
 func (s *Store) CountRules(ctx context.Context, flag storage.ResourceRequest) (count uint64, err error) {
-	return count, s.viewer.View(flag.Reference, func(ss storage.ReadOnlyStore) error {
+	return count, s.viewer.View(ctx, flag.Reference, func(ss storage.ReadOnlyStore) error {
 		count, err = ss.CountRules(ctx, flag)
 		return err
 	})
 }
 
 func (s *Store) GetSegment(ctx context.Context, p storage.ResourceRequest) (segment *flipt.Segment, err error) {
-	return segment, s.viewer.View(p.Reference, func(ss storage.ReadOnlyStore) error {
+	return segment, s.viewer.View(ctx, p.Reference, func(ss storage.ReadOnlyStore) error {
 		segment, err = ss.GetSegment(ctx, p)
 		return err
 	})
 }
 
 func (s *Store) ListSegments(ctx context.Context, req *storage.ListRequest[storage.NamespaceRequest]) (set storage.ResultSet[*flipt.Segment], err error) {
-	return set, s.viewer.View(req.Predicate.Reference, func(ss storage.ReadOnlyStore) error {
+	return set, s.viewer.View(ctx, req.Predicate.Reference, func(ss storage.ReadOnlyStore) error {
 		set, err = ss.ListSegments(ctx, req)
 		return err
 	})
 }
 
 func (s *Store) CountSegments(ctx context.Context, p storage.NamespaceRequest) (count uint64, err error) {
-	return count, s.viewer.View(p.Reference, func(ss storage.ReadOnlyStore) error {
+	return count, s.viewer.View(ctx, p.Reference, func(ss storage.ReadOnlyStore) error {
 		count, err = ss.CountSegments(ctx, p)
 		return err
 	})
 }
 
 func (s *Store) GetEvaluationRules(ctx context.Context, flag storage.ResourceRequest) (rules []*storage.EvaluationRule, err error) {
-	return rules, s.viewer.View(flag.Reference, func(ss storage.ReadOnlyStore) error {
+	return rules, s.viewer.View(ctx, flag.Reference, func(ss storage.ReadOnlyStore) error {
 		rules, err = ss.GetEvaluationRules(ctx, flag)
 		return err
 	})
 }
 
 func (s *Store) GetEvaluationDistributions(ctx context.Context, rule storage.IDRequest) (dists []*storage.EvaluationDistribution, err error) {
-	return dists, s.viewer.View(rule.Reference, func(ss storage.ReadOnlyStore) error {
+	return dists, s.viewer.View(ctx, rule.Reference, func(ss storage.ReadOnlyStore) error {
 		dists, err = ss.GetEvaluationDistributions(ctx, rule)
 		return err
 	})
 }
 
 func (s *Store) GetEvaluationRollouts(ctx context.Context, flag storage.ResourceRequest) (rollouts []*storage.EvaluationRollout, err error) {
-	return rollouts, s.viewer.View(flag.Reference, func(ss storage.ReadOnlyStore) error {
+	return rollouts, s.viewer.View(ctx, flag.Reference, func(ss storage.ReadOnlyStore) error {
 		rollouts, err = ss.GetEvaluationRollouts(ctx, flag)
 		return err
 	})
 }
 
 func (s *Store) GetNamespace(ctx context.Context, p storage.NamespaceRequest) (ns *flipt.Namespace, err error) {
-	return ns, s.viewer.View(p.Reference, func(ss storage.ReadOnlyStore) error {
+	return ns, s.viewer.View(ctx, p.Reference, func(ss storage.ReadOnlyStore) error {
 		ns, err = ss.GetNamespace(ctx, p)
 		return err
 	})
 }
 
 func (s *Store) ListNamespaces(ctx context.Context, req *storage.ListRequest[storage.ReferenceRequest]) (set storage.ResultSet[*flipt.Namespace], err error) {
-	return set, s.viewer.View(req.Predicate.Reference, func(ss storage.ReadOnlyStore) error {
+	return set, s.viewer.View(ctx, req.Predicate.Reference, func(ss storage.ReadOnlyStore) error {
 		set, err = ss.ListNamespaces(ctx, req)
 		return err
 	})
 }
 
 func (s *Store) CountNamespaces(ctx context.Context, req storage.ReferenceRequest) (count uint64, err error) {
-	return count, s.viewer.View(req.Reference, func(ss storage.ReadOnlyStore) error {
+	return count, s.viewer.View(ctx, req.Reference, func(ss storage.ReadOnlyStore) error {
 		count, err = ss.CountNamespaces(ctx, req)
 		return err
 	})
 }
 
 func (s *Store) GetRollout(ctx context.Context, p storage.NamespaceRequest, id string) (rollout *flipt.Rollout, err error) {
-	return rollout, s.viewer.View(p.Reference, func(ss storage.ReadOnlyStore) error {
+	return rollout, s.viewer.View(ctx, p.Reference, func(ss storage.ReadOnlyStore) error {
 		rollout, err = ss.GetRollout(ctx, p, id)
 		return err
 	})
 }
 
 func (s *Store) ListRollouts(ctx context.Context, req *storage.ListRequest[storage.ResourceRequest]) (set storage.ResultSet[*flipt.Rollout], err error) {
-	return set, s.viewer.View(req.Predicate.Reference, func(ss storage.ReadOnlyStore) error {
+	return set, s.viewer.View(ctx, req.Predicate.Reference, func(ss storage.ReadOnlyStore) error {
 		set, err = ss.ListRollouts(ctx, req)
 		return err
 	})
 }
 
 func (s *Store) CountRollouts(ctx context.Context, flag storage.ResourceRequest) (count uint64, err error) {
-	return count, s.viewer.View(flag.Reference, func(ss storage.ReadOnlyStore) error {
+	return count, s.viewer.View(ctx, flag.Reference, func(ss storage.ReadOnlyStore) error {
 		count, err = ss.CountRollouts(ctx, flag)
 		return err
 	})
