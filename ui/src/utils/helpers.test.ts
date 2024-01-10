c99d2566144550f441b9696f56278f2afb448513
Roman Dmytrenko <rdmytrenko@gmail.com>
2024-01-10 21:44:28 +0200

    refactor(ui): drop lodash as deps (#2640)

--- ui/src/utils/helpers.test.ts
+++ ui/src/utils/helpers.test.ts
@@ -1,4 +1,4 @@
-import { addNamespaceToPath } from './helpers';
+import { addNamespaceToPath, titleCase, upperFirst } from './helpers';
 
 describe('addNamespaceToPath', () => {
   it('should return a path with the namespace key', () => {
@@ -27,5 +27,19 @@ describe('addNamespaceToPath', () => {
     const key = 'test';
     const result = addNamespaceToPath(path, key);
     expect(result).toEqual('/namespaces/test/segments');
+  });
+});
+
+describe('upperFirst', () => {
+  it('should convert first char to upper case', () => {
+    const result = upperFirst('test is done');
+    expect(result).toEqual('Test is done');
+  });
+});
+
+describe('titleCase', () => {
+  it('should convert first char to upper case for each word', () => {
+    const result = titleCase('test is done');
+    expect(result).toEqual('Test Is Done');
   });
 });
