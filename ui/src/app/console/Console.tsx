7e030e17fb2c1f1b8cfbfa91dc39d6fafc37d1be
Roman Dmytrenko <rdmytrenko@gmail.com>
2024-01-12 15:15:14 +0200

    refactor(ui/console): replace monaco with codemirror (#2646)

--- ui/src/app/console/Console.tsx
+++ ui/src/app/console/Console.tsx
@@ -1,7 +1,9 @@
+import { json } from '@codemirror/lang-json';
 import { ArrowPathIcon } from '@heroicons/react/20/solid';
+import { tokyoNight } from '@uiw/codemirror-theme-tokyo-night';
+import CodeMirror from '@uiw/react-codemirror';
 import { Form, Formik, useFormikContext } from 'formik';
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
@@ -57,8 +59,6 @@ export default function Console() {
   const { setSuccess } = useSuccess();
 
   const namespace = useSelector(selectCurrentNamespace);
-
-  const codeRef = useRef<HTMLElement>(null);
 
   const { data, error } = useListFlagsQuery(namespace.key);
 
@@ -164,15 +164,6 @@ export default function Console() {
     setSuccess('Command copied to clipboard');
   };
 
-  useEffect(() => {
-    if (codeRef.current) {
-      monaco.editor.colorizeElement(codeRef.current, {
-        mimeType: 'json',
-        theme: 'tmrw'
-      });
-    }
-  }, [response, codeRef]);
-
   const initialvalues: ConsoleFormValues = {
     flagKey: selectedFlag?.key || '',
     entityId: uuidv4(),
@@ -259,7 +250,7 @@ export default function Console() {
                           >
                             Request Context
                           </label>
-                          <div className="nightwind-prevent mt-1">
+                          <div className="nightwind-prevent mt-1 text-sm">
                             <ContextEditor
                               id="context"
                               setValue={(v) => {
@@ -297,13 +288,22 @@ export default function Console() {
             </div>
             <div className="mt-8 w-full overflow-hidden md:w-1/2 md:pl-4">
               {response && (
-                <pre className="monaco-editor p-2 text-sm md:h-full">
+                <pre className="nightwind-prevent bg-[#1a1b26] p-2 text-sm md:h-full">
                   {hasEvaluationError ? (
                     <p className="text-red-400">{response}</p>
                   ) : (
-                    <code className="json rounded-sm md:h-full" ref={codeRef}>
-                      {response as React.ReactNode}
-                    </code>
+                    <CodeMirror
+                      value={response}
+                      height="100%"
+                      extensions={[json()]}
+                      basicSetup={{
+                        lineNumbers: false,
+                        foldGutter: false,
+                        highlightActiveLine: false
+                      }}
+                      editable={false}
+                      theme={tokyoNight}
+                    />
                   )}
                 </pre>
               )}
--- ui/src/components/console/ContextEditor.tsx
+++ ui/src/components/console/ContextEditor.tsx
@@ -1,11 +1,8 @@
-import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-import 'monaco-editor/esm/vs/language/json/monaco.contribution';
-import tmrw from 'monaco-themes/themes/Tomorrow-Night-Bright.json';
-import { useEffect, useRef } from 'react';
-import styles from './ContextEditor.module.css';
-import './worker';
-
-monaco.editor.defineTheme('tmrw', tmrw as monaco.editor.IStandaloneThemeData);
+import { json, jsonParseLinter } from '@codemirror/lang-json';
+import { linter } from '@codemirror/lint';
+import { tokyoNight } from '@uiw/codemirror-theme-tokyo-night';
+import CodeMirror from '@uiw/react-codemirror';
+import React from 'react';
 
 type ContextEditorProps = {
   id: string;
@@ -15,40 +12,27 @@ type ContextEditorProps = {
 export const ContextEditor: React.FC<ContextEditorProps> = (
   props: ContextEditorProps
 ) => {
-  const { id, setValue } = props;
-
-  const monacoEl = useRef(null);
-
-  useEffect(() => {
-    if (monacoEl.current) {
-      const editor = monaco.editor.create(monacoEl.current!, {
-        value: '{}',
-        language: 'json',
-        fontSize: 14,
-        lineNumbers: 'off',
-        renderLineHighlight: 'none',
-        minimap: {
-          enabled: false
-        },
-        folding: false,
-        autoDetectHighContrast: false,
-        autoClosingBrackets: 'always',
-        scrollBeyondLastLine: false,
-        theme: 'tmrw'
-      });
-
-      // After initializing monaco editor
-      const subscription = editor.onDidChangeModelContent(() => {
-        setValue(editor.getValue());
-      });
-
-      return () => {
-        subscription.dispose();
-        editor.dispose();
-      };
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return <div id={id} className={styles.ContextEditor} ref={monacoEl}></div>;
+  const { setValue } = props;
+  const onChange = React.useCallback(
+    (val: any, _: any) => {
+      setValue(val);
+    },
+    [setValue]
+  );
+  return (
+    <CodeMirror
+      value="{}"
+      height="50vh"
+      extensions={[json(), linter(jsonParseLinter())]}
+      onChange={onChange}
+      basicSetup={{
+        lineNumbers: false,
+        foldGutter: false,
+        highlightActiveLineGutter: true,
+        autocompletion: true,
+        highlightActiveLine: true
+      }}
+      theme={tokyoNight}
+    />
+  );
 };
