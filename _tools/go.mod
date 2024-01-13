af8be205ef43dd5f02cec5bbcace7baae122c8b9
Roman Dmytrenko <rdmytrenko@gmail.com>
2024-01-13 16:08:04 +0200

    chore(deps): drop golang.org/x/tools/cmd/cover v0.1.0-deprecated

--- _tools/go.mod
+++ _tools/go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.0
 	github.com/rakyll/gotest v0.0.6
 	golang.org/x/tools v0.16.1
-	golang.org/x/tools/cmd/cover v0.1.0-deprecated
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.3.0
 	google.golang.org/protobuf v1.32.0
 )
--- _tools/tools.go
+++ _tools/tools.go
@@ -11,7 +11,6 @@ import (
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
 	_ "github.com/rakyll/gotest"
-	_ "golang.org/x/tools/cmd/cover"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
