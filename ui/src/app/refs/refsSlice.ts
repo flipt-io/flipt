36b27fe75fc07ab6c37923225312dc15ac07a269
Mark Phelps <209477+markphelps@users.noreply.github.com>
2024-01-31 12:13:55 -0500

    feat: UI refs pt1 (#2727)
    * feat(wip): beginnings of being able to pick a branch in the UI
    
    * refactor: clean up reference in http request (#2724)
    
    * refactor: clean up reference in http request
    
    * address pr feedback
    
    ---------
    
    Co-authored-by: Roman Dmytrenko <rdmytrenko@gmail.com>

---
+++ ui/src/app/refs/refsSlice.ts
@@ -0,0 +1,31 @@
+import { createSelector, createSlice } from '@reduxjs/toolkit';
+import { RootState } from '~/store';
+
+export const refsKey = 'refs';
+interface IRefState {
+  currentRef?: string;
+}
+
+const initialState: IRefState = {
+  currentRef: undefined
+};
+
+export const refsSlice = createSlice({
+  name: 'refs',
+  initialState,
+  reducers: {
+    currentRefChanged: (state, action) => {
+      const ref = action.payload;
+      state.currentRef = ref;
+    }
+  }
+});
+
+export const { currentRefChanged } = refsSlice.actions;
+
+export const selectCurrentRef = createSelector(
+  [(state: RootState) => state.refs.currentRef],
+  (ref) => ref
+);
+
+export default refsSlice.reducer;
