feabe7483e80fc290dc2ab002d98f13e04f5135d
Roman Dmytrenko <rdmytrenko@gmail.com>
2024-03-22 23:33:30 +0200

    cleanup

--- ui/src/components/forms/Tags.tsx
+++ ui/src/components/forms/Tags.tsx
@@ -1,7 +1,7 @@
 import { useField } from 'formik';
 import { cls } from '~/utils/helpers';
 import { TrashIcon } from '@heroicons/react/24/outline';
-import React, { KeyboardEvent } from 'react';
+import React, { KeyboardEvent, useState } from 'react';
 
 type TagsProps = {
   id: string;
@@ -20,7 +20,7 @@ export default function Tags(props: TagsProps) {
   const { id, type = 'text', forwardRef, placeholder } = props;
   const [field, meta] = useField(props);
   const hasError = !!(meta.touched && meta.error);
-
+  const [inputValue, setInputValue] = useState('');
   let tags: Item[] = [];
   try {
     const v = JSON.parse(field.value);
@@ -31,7 +31,7 @@ export default function Tags(props: TagsProps) {
     // todo - what could be done?
   }
 
-  const setValue = (newTags: Item[]) => {
+  const setFieldValue = (newTags: Item[]) => {
     let v = '';
     if (newTags.length != 0) {
       v = JSON.stringify(newTags);
@@ -40,31 +40,32 @@ export default function Tags(props: TagsProps) {
     field.onBlur(e);
     field.onChange(e);
   };
+
+  const addTag = () => {
+    let val: Item = inputValue.trim();
+    if (type == 'number') {
+      val = Number(val);
+    }
+    if (!val || tags.indexOf(val) != -1) {
+      return;
+    }
+    setFieldValue([...tags, val]);
+    setInputValue('');
+  };
+
   const removeTag = (i: number) => {
     const newTags = [...tags];
     newTags.splice(i, 1);
-    setValue(newTags);
-  };
-
-  const toValue = (val: any): Item => {
-    if (type == 'number') {
-      return Number(val);
-    }
-    return val;
+    setFieldValue(newTags);
   };
 
   const inputKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-    const val = toValue(e.currentTarget.value);
-    if (e.code === 'Enter' && val) {
+    if (e.code === 'Enter' && e.currentTarget.value) {
       e.preventDefault();
-      if (tags.indexOf(val) != -1) {
-        return;
-      }
-      setValue([...tags, val]);
-      e.currentTarget.value = '';
+      addTag();
     }
   };
-  console.log(hasError, meta);
+
   return (
     <>
       <div ref={forwardRef} id={id}>
@@ -73,9 +74,11 @@ export default function Tags(props: TagsProps) {
             {tags.map((tag, i) => (
               <li
                 key={tag}
-                className="text-white bg-gray-500 flex flex-row items-center justify-center rounded px-2 py-1 align-text-top text-sm font-medium"
+                className="text-gray-600 bg-gray-100 flex flex-row items-center justify-center rounded px-2 py-1 align-text-top text-sm font-medium"
               >
-                <span className="leading-relaxed">{tag}</span>
+                <span className="max-w-32 truncate" title={String(tag)}>
+                  {tag}
+                </span>
                 <button
                   className="ml-1 p-1"
                   type="button"
@@ -90,12 +93,6 @@ export default function Tags(props: TagsProps) {
           </ul>
         )}
         <div className="relative flex w-full">
-          <button
-            className="z-1 border-1 bg-white text-gray-500 border-violet-300 !absolute right-1 top-1 select-none rounded border px-4 py-1.5 text-center align-middle text-xs font-bold"
-            type="button"
-          >
-            Add
-          </button>
           <input
             className={cls(
               'text-gray-900 bg-gray-50 border-gray-300 block w-full rounded-md shadow-sm focus:border-violet-300 disabled:text-gray-500 disabled:bg-gray-100 disabled:border-gray-200 focus:ring-violet-300 disabled:cursor-not-allowed sm:text-sm',
@@ -106,7 +103,18 @@ export default function Tags(props: TagsProps) {
             type={type}
             placeholder={placeholder}
             onKeyDown={inputKeyDown}
+            value={inputValue}
+            onChange={(e) => {
+              setInputValue(e.currentTarget.value);
+            }}
           />
+          <button
+            className="z-1 border-1 bg-white text-gray-500 border-violet-300 !absolute right-1 top-1 select-none rounded border px-4 py-2 text-center align-middle text-xs font-bold"
+            type="button"
+            onClick={addTag}
+          >
+            Add
+          </button>
         </div>
       </div>
       {hasError && meta.error?.length && meta.error.length > 0 ? (
