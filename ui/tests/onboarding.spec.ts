3f3e9b2b9ed716e1708ce8d0e271ed2d44affb03
Mark Phelps <209477+markphelps@users.noreply.github.com>
2024-05-13 15:39:55 -0400

    chore: use role selector in onboarding specs
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- ui/tests/onboarding.spec.ts
+++ ui/tests/onboarding.spec.ts
@@ -7,27 +7,29 @@ test.describe('Onboarding', () => {
 
       await expect(page.locator('h1')).toContainText('Onboarding');
       await expect(
-        page.getByText('Get Started', { exact: true })
+        page.getByRole('heading', { name: 'Get Started' })
       ).toBeVisible();
       await expect(
-        page.getByText('Introducing Flipt Hybrid Cloud', { exact: true })
+        page.getByRole('heading', { name: 'Introducing Flipt Hybrid Cloud' })
       ).toBeVisible();
       await expect(
-        page.getByText('Try the CLI', { exact: true })
+        page.getByRole('heading', { name: 'Try the CLI' })
       ).toBeVisible();
       await expect(
-        page.getByText('Checkout a Guide', { exact: true })
+        page.getByRole('heading', { name: 'Checkout a Guide' })
       ).toBeVisible();
       await expect(
-        page.getByText('Integrate Your Application', { exact: true })
+        page.getByRole('heading', { name: 'Integrate Your Application' })
       ).toBeVisible();
       await expect(
-        page.getByText('Join the Community', { exact: true })
+        page.getByRole('heading', { name: 'Join the Community' })
       ).toBeVisible();
       await expect(
-        page.getByText('View API Reference', { exact: true })
+        page.getByRole('heading', { name: 'View API Reference' })
       ).toBeVisible();
-      await expect(page.getByText('Support Us', { exact: true })).toBeVisible();
+      await expect(
+        page.getByRole('heading', { name: 'Support Us' })
+      ).toBeVisible();
       await expect(
         page.getByRole('button', { name: 'Continue to Dashboard' })
       ).toBeVisible();
