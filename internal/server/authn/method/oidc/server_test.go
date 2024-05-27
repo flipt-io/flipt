e9112c07d1aa681f24d2184d415e758c0c358938
Mark Phelps <209477+markphelps@users.noreply.github.com>
2024-05-27 13:21:30 -0400

    chore: fix authn oidc server test
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- internal/server/authn/method/oidc/server_test.go
+++ internal/server/authn/method/oidc/server_test.go
@@ -344,15 +344,19 @@ func testOIDCFlow(t *testing.T, ctx context.Context, tpAddr, clientAddress strin
 
 		assert.Empty(t, response.ClientToken) // middleware moves it to cookie
 		assert.Equal(t, auth.Method_METHOD_OIDC, response.Authentication.Method)
-		assert.Equal(t, map[string]string{
+
+		for k, v := range map[string]string{
 			"io.flipt.auth.oidc.provider": "google",
 			"io.flipt.auth.oidc.email":    "mark@flipt.io",
 			"io.flipt.auth.email":         "mark@flipt.io",
 			"io.flipt.auth.oidc.name":     "Mark Phelps",
 			"io.flipt.auth.name":          "Mark Phelps",
 			"io.flipt.auth.oidc.sub":      "mark",
-			"io.flipt.auth.role":          "admin",
-		}, response.Authentication.Metadata)
+		} {
+			assert.Equal(t, v, response.Authentication.Metadata[k])
+		}
+
+		assert.NotEmpty(t, response.Authentication.Metadata["io.flipt.auth.claims"])
 
 		// ensure expiry is set
 		assert.NotNil(t, response.Authentication.ExpiresAt)
