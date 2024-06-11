4e9278136c4395e492f452608b92348a9f47b8dc
Mark Phelps <209477+markphelps@users.noreply.github.com>
2024-06-11 19:04:04 -0400

    chore: try to fix migrations
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- config/migrations/cockroachdb/10_variants_default.up.sql
+++ config/migrations/cockroachdb/10_variants_default.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE variants ADD COLUMN `default` BOOLEAN DEFAULT FALSE NOT NULL;
-CREATE UNIQUE INDEX variants_default_unique_index ON variants (namespace_key, flag_key) WHERE `default` = TRUE;
+ALTER TABLE variants ADD COLUMN default BOOLEAN DEFAULT FALSE NOT NULL;
+CREATE UNIQUE INDEX variants_default_unique_index ON variants (namespace_key, flag_key) WHERE default = TRUE;
