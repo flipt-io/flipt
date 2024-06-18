c98d8721e6ef444db9a937e335892e46e3ff5166
Mark Phelps <209477+markphelps@users.noreply.github.com>
2024-06-18 19:30:10 -0400

    chore: fix tests
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- internal/server/authn/server_test.go
+++ internal/server/authn/server_test.go
@@ -18,9 +18,7 @@ import (
 	rpcauth "go.flipt.io/flipt/rpc/flipt/auth"
 	"go.uber.org/zap/zaptest"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
-	"google.golang.org/grpc/status"
 	"google.golang.org/grpc/test/bufconn"
 	"google.golang.org/protobuf/testing/protocmp"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -103,7 +101,8 @@ func TestServer(t *testing.T) {
 
 	t.Run("GetAuthenticationSelf", func(t *testing.T) {
 		_, err := client.GetAuthenticationSelf(ctx, &emptypb.Empty{})
-		require.ErrorIs(t, err, status.Error(codes.Unauthenticated, "request was not authenticated"))
+		uerr := errors.ErrUnauthenticatedf("request was not authenticated")
+		require.ErrorAs(t, err, &uerr)
 
 		retrievedAuth, err := client.GetAuthenticationSelf(authorize(ctx), &emptypb.Empty{})
 		require.NoError(t, err)
@@ -159,7 +158,8 @@ func TestServer(t *testing.T) {
 
 		// get self with authenticated context now unauthorized
 		_, err = client.GetAuthenticationSelf(ctx, &emptypb.Empty{})
-		require.ErrorIs(t, err, status.Error(codes.Unauthenticated, "request was not authenticated"))
+		uerr := errors.ErrUnauthenticatedf("request was not authenticated")
+		require.ErrorAs(t, err, &uerr)
 
 		// no longer can be retrieved from store by client ID
 		_, err = store.GetAuthenticationByClientToken(ctx, clientToken)
@@ -195,7 +195,8 @@ func TestServer(t *testing.T) {
 
 		// get self with authenticated context now unauthorized
 		_, err = client.GetAuthenticationSelf(ctx, &emptypb.Empty{})
-		require.ErrorIs(t, err, status.Error(codes.Unauthenticated, "request was not authenticated"))
+		uerr := errors.ErrUnauthenticatedf("request was not authenticated")
+		require.ErrorAs(t, err, &uerr)
 	})
 }
 
