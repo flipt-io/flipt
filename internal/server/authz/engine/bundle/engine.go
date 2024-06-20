8ba3cee633576c77673293986e6b05ea3bba69cf
Mark Phelps <209477+markphelps@users.noreply.github.com>
2024-06-20 10:07:37 -0400

    chore: set AWS_REGION for user if not set
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- internal/server/authz/engine/bundle/engine.go
+++ internal/server/authz/engine/bundle/engine.go
@@ -2,6 +2,7 @@ package bundle
 
 import (
 	"context"
+	"os"
 	"strings"
 
 	"github.com/open-policy-agent/contrib/logging/plugins/ozap"
@@ -14,17 +15,36 @@ import (
 
 var _ authz.Verifier = (*Engine)(nil)
 
+type cleanupFunc func()
+
 type Engine struct {
-	opa    *sdk.OPA
-	logger *zap.Logger
+	opa          *sdk.OPA
+	logger       *zap.Logger
+	cleanupFuncs []cleanupFunc
 }
 
 func NewEngine(ctx context.Context, logger *zap.Logger, cfg *config.Config) (*Engine, error) {
-	var opaConfig string
+	var (
+		opaConfig    string
+		cleanupFuncs []cleanupFunc
+	)
 
 	switch cfg.Authorization.Backend {
 	case config.AuthorizationBackendObject:
 		opaConfig = cfg.Authorization.Object.String()
+
+		switch cfg.Authorization.Object.Type { //nolint
+		case config.S3ObjectAuthorizationBackendType:
+			// set AWS_REGION env var if not set and region is specified
+			// this is a nicety as the OPA env credentials provider requires this env var
+			// to be set, but we don't want the user to have to supply it twice if they already have it in the config
+			if cfg.Authorization.Object.S3.Region != "" && os.Getenv("AWS_REGION") == "" {
+				os.Setenv("AWS_REGION", cfg.Authorization.Object.S3.Region)
+				cleanupFuncs = append(cleanupFuncs, func() {
+					os.Unsetenv("AWS_REGION")
+				})
+			}
+		}
 	case config.AuthorizationBackendBundle:
 		opaConfig = cfg.Authorization.Bundle.String()
 	}
@@ -44,8 +64,9 @@ func NewEngine(ctx context.Context, logger *zap.Logger, cfg *config.Config) (*En
 	}
 
 	return &Engine{
-		logger: logger,
-		opa:    opa,
+		logger:       logger,
+		opa:          opa,
+		cleanupFuncs: cleanupFuncs,
 	}, nil
 }
 
@@ -70,5 +91,8 @@ func (e *Engine) IsAllowed(ctx context.Context, input map[string]interface{}) (b
 
 func (e *Engine) Shutdown(ctx context.Context) error {
 	e.opa.Stop(ctx)
+	for _, cleanup := range e.cleanupFuncs {
+		cleanup()
+	}
 	return nil
 }
