ca2d447ba76154ebf94e35770c2f121148cd3d38
Mark Phelps <209477+markphelps@users.noreply.github.com>
2024-08-01 16:14:41 -0400

    chore: rm screenshot pipeline
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- ui/screenshot/concepts/rules.js
+++
@@ -1,9 +0,0 @@
-import { capture, scrollToBottom } from '../../screenshot.js';
-
-(async () => {
-  await capture('concepts', 'rules', async (page) => {
-    await page.getByRole('link', { name: 'colorscheme' }).click();
-    await page.getByRole('link', { name: 'Rules' }).click();
-    await scrollToBottom(page);
-  });
-})();
