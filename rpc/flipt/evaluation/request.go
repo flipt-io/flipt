9bc86ece5b52e60e6b56bafbc50ce215248a2bd8
George MacRorie <me@georgemac.com>
2024-08-15 09:40:26 +0100

    fix(rpc/flipt): implement Requester for evaluation data snapshot
    Signed-off-by: George MacRorie <me@georgemac.com>

---
+++ rpc/flipt/evaluation/request.go
@@ -0,0 +1,10 @@
+package evaluation
+
+import "go.flipt.io/flipt/rpc/flipt"
+
+func (r *EvaluationNamespaceSnapshotRequest) Request() []flipt.Request {
+	return []flipt.Request{
+		flipt.NewRequest(flipt.ResourceFlag, flipt.ActionRead, flipt.WithNamespace(r.Key)),
+		flipt.NewRequest(flipt.ResourceSegment, flipt.ActionRead, flipt.WithNamespace(r.Key)),
+	}
+}
