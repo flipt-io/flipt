32bc5fed2839f9ec010241dc3b0c2972da51d8f1
Roman Dmytrenko <rdmytrenko@gmail.com>
2024-11-07 20:59:36 +0200

    fix few violet colors
    Signed-off-by: Roman Dmytrenko <rdmytrenko@gmail.com>

--- ui/src/components/Banner.tsx
+++ ui/src/components/Banner.tsx
@@ -14,8 +14,8 @@ export default function Banner(props: BannerProps) {
   const dispatch = useDispatch();
 
   return (
-    <div className="bg-violet-700 flex items-center gap-x-6 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
-      <p className="text-white text-sm leading-6">
+    <div className="flex items-center gap-x-6 bg-violet-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
+      <p className="text-sm leading-6 text-white">
         <a href={href}>
           <strong className="font-semibold">{title}</strong>
           <svg
@@ -36,7 +36,7 @@ export default function Banner(props: BannerProps) {
           onClick={() => dispatch(bannerDismissed())}
         >
           <span className="sr-only">Dismiss</span>
-          <XMarkIcon className="text-white h-5 w-5" aria-hidden="true" />
+          <XMarkIcon className="h-5 w-5 text-white" aria-hidden="true" />
         </button>
       </div>
     </div>
