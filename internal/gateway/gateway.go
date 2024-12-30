7357abd3175f1294c9c0c7f0789f42e148d7d889
Roman Dmytrenko <rdmytrenko@gmail.com>
2024-12-31 00:12:55 +0200

    fix(grpc-gateway): rework support for flipt.NewV1toV2MarshallerAdapter (#3757)
    * bump related dependencies
    
    Signed-off-by: Roman Dmytrenko <rdmytrenko@gmail.com>
    
    * fix(grpc-gateway): rework support for flipt.NewV1toV2MarshallerAdapter
    
    Signed-off-by: Roman Dmytrenko <rdmytrenko@gmail.com>
    
    * mage proto
    
    Signed-off-by: Roman Dmytrenko <rdmytrenko@gmail.com>
    
    ---------
    
    Signed-off-by: Roman Dmytrenko <rdmytrenko@gmail.com>

--- internal/gateway/gateway.go
+++ internal/gateway/gateway.go
@@ -4,7 +4,6 @@ import (
 	"sync"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	"go.flipt.io/flipt/rpc/flipt"
 	"go.uber.org/zap"
 	"google.golang.org/protobuf/encoding/protojson"
 )
@@ -13,17 +12,17 @@ import (
 // This is required to fix a backwards compatibility issue with the v2 marshaller where `null` map values
 // cause an error because they are not allowed by the proto spec, but they were handled by the v1 marshaller.
 //
-// See: rpc/flipt/marshal.go
-//
 // See: https://github.com/flipt-io/flipt/issues/664
-var commonMuxOptions []runtime.ServeMuxOption
-var once sync.Once
+var (
+	commonMuxOptions []runtime.ServeMuxOption
+	once             sync.Once
+)
 
 // NewGatewayServeMux builds a new gateway serve mux with common options.
 func NewGatewayServeMux(logger *zap.Logger, opts ...runtime.ServeMuxOption) *runtime.ServeMux {
 	once.Do(func() {
 		commonMuxOptions = []runtime.ServeMuxOption{
-			runtime.WithMarshalerOption(runtime.MIMEWildcard, flipt.NewV1toV2MarshallerAdapter(logger)),
+			runtime.WithMarshalerOption(runtime.MIMEWildcard, NewV1toV2MarshallerAdapter(logger)),
 			runtime.WithMarshalerOption("application/json+pretty", &runtime.JSONPb{
 				MarshalOptions: protojson.MarshalOptions{
 					Indent:    "  ",
@@ -34,7 +33,6 @@ func NewGatewayServeMux(logger *zap.Logger, opts ...runtime.ServeMuxOption) *run
 				},
 			}),
 		}
-
 	})
 
 	return runtime.NewServeMux(append(opts, commonMuxOptions...)...)
