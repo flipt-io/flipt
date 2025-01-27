afd953150b2524af8323acd569fa9aa417135db9
George <me@georgemac.com>
2025-01-27 16:13:58 +0000

    refactor(internal/server): remove unused server implementations and remove v2 folder (#3851)

--- internal/server/flag.go
+++ internal/server/flag.go
@@ -3,37 +3,10 @@ package server
 import (
 	"context"
 
-	fliptotel "go.flipt.io/flipt/internal/server/otel"
 	"go.flipt.io/flipt/internal/storage"
 	flipt "go.flipt.io/flipt/rpc/flipt"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
-	empty "google.golang.org/protobuf/types/known/emptypb"
 )
-
-// GetFlag gets a flag
-func (s *Server) GetFlag(ctx context.Context, r *flipt.GetFlagRequest) (*flipt.Flag, error) {
-	s.logger.Debug("get flag", zap.Stringer("request", r))
-
-	flag, err := s.store.GetFlag(ctx, storage.NewResource(r.NamespaceKey, r.Key, storage.WithReference(r.Reference)))
-
-	spanAttrs := []attribute.KeyValue{
-		fliptotel.AttributeNamespace.String(r.NamespaceKey),
-		fliptotel.AttributeFlag.String(r.Key),
-	}
-
-	if flag != nil {
-		spanAttrs = append(spanAttrs, fliptotel.AttributeFlagEnabled.Bool(flag.Enabled))
-	}
-
-	// add otel attributes to span
-	span := trace.SpanFromContext(ctx)
-	span.SetAttributes(spanAttrs...)
-
-	s.logger.Debug("get flag", zap.Stringer("response", flag))
-	return flag, err
-}
 
 // ListFlags lists all flags
 func (s *Server) ListFlags(ctx context.Context, r *flipt.ListFlagRequest) (*flipt.FlagList, error) {
@@ -45,8 +18,44 @@ func (s *Server) ListFlags(ctx context.Context, r *flipt.ListFlagRequest) (*flip
 		return nil, err
 	}
 
-	resp := flipt.FlagList{
-		Flags: results.Results,
+	resp := flipt.FlagList{}
+
+	for _, flag := range results.Results {
+		var defaultVariant *flipt.Variant
+		if flag.DefaultVariant != nil {
+			for _, variant := range flag.Variants {
+				if *flag.DefaultVariant != variant.Key {
+					continue
+				}
+
+				var attachment string
+				if variant.Attachment != nil {
+					v, err := variant.Attachment.MarshalJSON()
+					if err != nil {
+						return nil, err
+					}
+
+					attachment = string(v)
+				}
+
+				defaultVariant = &flipt.Variant{
+					FlagKey:      flag.Key,
+					NamespaceKey: r.NamespaceKey,
+					Key:          variant.Key,
+					Name:         variant.Name,
+					Description:  variant.Description,
+					Attachment:   attachment,
+				}
+				break
+			}
+		}
+
+		resp.Flags = append(resp.Flags, &flipt.Flag{
+			Key:            flag.Key,
+			Name:           flag.Name,
+			Description:    flag.Description,
+			DefaultVariant: defaultVariant,
+		})
 	}
 
 	total, err := s.store.CountFlags(ctx, ns)
@@ -59,54 +68,4 @@ func (s *Server) ListFlags(ctx context.Context, r *flipt.ListFlagRequest) (*flip
 
 	s.logger.Debug("list flags", zap.Stringer("response", &resp))
 	return &resp, nil
-}
-
-// CreateFlag creates a flag
-func (s *Server) CreateFlag(ctx context.Context, r *flipt.CreateFlagRequest) (*flipt.Flag, error) {
-	s.logger.Debug("create flag", zap.Stringer("request", r))
-	flag, err := s.store.CreateFlag(ctx, r)
-	s.logger.Debug("create flag", zap.Stringer("response", flag))
-	return flag, err
-}
-
-// UpdateFlag updates an existing flag
-func (s *Server) UpdateFlag(ctx context.Context, r *flipt.UpdateFlagRequest) (*flipt.Flag, error) {
-	s.logger.Debug("update flag", zap.Stringer("request", r))
-	flag, err := s.store.UpdateFlag(ctx, r)
-	s.logger.Debug("update flag", zap.Stringer("response", flag))
-	return flag, err
-}
-
-// DeleteFlag deletes a flag
-func (s *Server) DeleteFlag(ctx context.Context, r *flipt.DeleteFlagRequest) (*empty.Empty, error) {
-	s.logger.Debug("delete flag", zap.Stringer("request", r))
-	if err := s.store.DeleteFlag(ctx, r); err != nil {
-		return nil, err
-	}
-	return &empty.Empty{}, nil
-}
-
-// CreateVariant creates a variant
-func (s *Server) CreateVariant(ctx context.Context, r *flipt.CreateVariantRequest) (*flipt.Variant, error) {
-	s.logger.Debug("create variant", zap.Stringer("request", r))
-	variant, err := s.store.CreateVariant(ctx, r)
-	s.logger.Debug("create variant", zap.Stringer("response", variant))
-	return variant, err
-}
-
-// UpdateVariant updates an existing variant
-func (s *Server) UpdateVariant(ctx context.Context, r *flipt.UpdateVariantRequest) (*flipt.Variant, error) {
-	s.logger.Debug("update variant", zap.Stringer("request", r))
-	variant, err := s.store.UpdateVariant(ctx, r)
-	s.logger.Debug("update variant", zap.Stringer("response", variant))
-	return variant, err
-}
-
-// DeleteVariant deletes a variant
-func (s *Server) DeleteVariant(ctx context.Context, r *flipt.DeleteVariantRequest) (*empty.Empty, error) {
-	s.logger.Debug("delete variant", zap.Stringer("request", r))
-	if err := s.store.DeleteVariant(ctx, r); err != nil {
-		return nil, err
-	}
-	return &empty.Empty{}, nil
 }
