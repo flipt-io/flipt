e58800f2885280f7abff714def54f3d7f4227757
George MacRorie <me@georgemac.com>
2025-02-11 14:29:35 +0000

    feat(ui): support larger screen widths

--- ui/src/app/Layout.tsx
+++ ui/src/app/Layout.tsx
@@ -92,7 +92,7 @@ function InnerLayout() {
           />
         )}
         <main className="flex pt-1 sm:pt-4">
-          <div className="mx-auto w-full max-w-screen-lg overflow-x-auto px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto w-full lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl overflow-x-auto px-4 sm:px-6 lg:px-8">
             <Outlet />
           </div>
         </main>
