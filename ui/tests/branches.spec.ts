20bcd5df2d0b80fa1359afcd976b8c2d4c8b7670
Mark Phelps <209477+markphelps@users.noreply.github.com>
2025-05-29 15:32:35 -0400

    chore: add branch delete IT
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- ui/tests/branches.spec.ts
+++ ui/tests/branches.spec.ts
@@ -20,8 +20,49 @@ test.describe('Branches', () => {
       .getByRole('button', { name: 'foo' })
       .click();
     await page
-      .getByTestId('environment-listbox')
+      .getByTestId('namespace-listbox')
       .getByRole('button', { name: 'default' })
       .click();
+  });
+
+  test('cannot delete branch if not confirmed', async ({ page }) => {
+    await page
+      .getByTestId('environment-namespace-switcher')
+      .getByRole('button')
+      .click();
+    await page
+      .getByTestId('environment-listbox')
+      .getByRole('button', { name: 'foo' })
+      .click();
+    await page
+      .getByTestId('namespace-listbox')
+      .getByRole('button', { name: 'default' })
+      .click();
+    await page.getByText('Branched from: local').click();
+    await page.getByRole('menuitem', { name: 'Delete branch' }).click();
+    await page.getByRole('textbox', { name: 'foo' }).fill('bar');
+    await expect(
+      page.getByRole('button', { name: 'Delete branch' })
+    ).toBeDisabled();
+  });
+
+  test('can delete branch', async ({ page }) => {
+    await page
+      .getByTestId('environment-namespace-switcher')
+      .getByRole('button')
+      .click();
+    await page
+      .getByTestId('environment-listbox')
+      .getByRole('button', { name: 'foo' })
+      .click();
+    await page
+      .getByTestId('namespace-listbox')
+      .getByRole('button', { name: 'default' })
+      .click();
+    await page.getByText('Branched from: local').click();
+    await page.getByRole('menuitem', { name: 'Delete branch' }).click();
+    await page.getByRole('textbox', { name: 'foo' }).fill('foo');
+    await page.getByRole('button', { name: 'Delete branch' }).click();
+    await expect(page.getByText('Branch deleted successfully')).toBeVisible();
   });
 });
