cecdb31ad6d5570e04b0bf0bfad52e017d34f01a
Roman Dmytrenko <rdmytrenko@gmail.com>
2025-06-01 21:36:23 +0000

    fix: v2 fix templates for propose changes (#4274)

--- internal/storage/fs/config_test.go
+++ internal/storage/fs/config_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.flipt.io/flipt/internal/server/environments"
+	api "go.flipt.io/flipt/rpc/v2/environments"
 )
 
 func Test_DefaultFliptConfig(t *testing.T) {
@@ -68,8 +69,72 @@ create segment default/some-other-segment`,
 				require.NoError(t, err)
 
 				assert.Equal(t, test.expected, buf.String())
-
 			})
 		}
 	})
+
+	t.Run("ProposalTitleTemplate", func(t *testing.T) {
+		dir := "qwerty"
+		tests := []struct {
+			name          string
+			input         templateContext
+			expectedTitle string
+			expectedBody  string
+		}{
+			{
+				"no directory",
+				templateContext{
+					Base:   &api.EnvironmentConfiguration{Ref: "main"},
+					Branch: &api.EnvironmentConfiguration{Ref: "some-branch"},
+				},
+				"Flipt: Update features on main",
+				`This pull request updates Flipt resources on branch main.
+
+🟢 **Source:**
+- Branch: some-branch
+
+🎯 **Target:**
+- Branch: main
+
+👀 Please review the changes and merge if everything looks good.`,
+			},
+			{
+				"defined directory",
+				templateContext{
+					Base:   &api.EnvironmentConfiguration{Ref: "main", Directory: &dir},
+					Branch: &api.EnvironmentConfiguration{Ref: "some-branch", Directory: &dir},
+				},
+				"Flipt: Update features in qwerty on main",
+				`This pull request updates Flipt resources in qwerty on branch main.
+
+🟢 **Source:**
+- Directory: qwerty
+- Branch: some-branch
+
+🎯 **Target:**
+- Directory: qwerty
+- Branch: main
+
+👀 Please review the changes and merge if everything looks good.`,
+			},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				buf := &bytes.Buffer{}
+				err := conf.Templates.ProposalTitleTemplate.Execute(buf, tt.input)
+				require.NoError(t, err)
+				assert.Equal(t, tt.expectedTitle, buf.String())
+				buf.Reset()
+				err = conf.Templates.ProposalBodyTemplate.Execute(buf, tt.input)
+				require.NoError(t, err)
+				assert.Equal(t, tt.expectedBody, buf.String())
+			})
+		}
+	})
+}
+
+type templateContext struct {
+	Base   *api.EnvironmentConfiguration
+	Branch *api.EnvironmentConfiguration
 }
