7a68123b684e3a9a52288cb3c566d1b15e319101
Mark Phelps <209477+markphelps@users.noreply.github.com>
2025-06-18 17:43:06 -0400

    V2 quickstart cmd (#4362)

---
+++ internal/cmd/util/browser.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"os/exec"
+	"runtime"
+)
+
+// OpenBrowser opens the specified URL in the default browser of the user.
+func OpenBrowser(url string) error {
+	var (
+		cmd  string
+		args []string
+	)
+
+	switch runtime.GOOS {
+	case "darwin":
+		cmd = "open"
+	case "windows":
+		cmd = "rundll32"
+		args = append(args, "url.dll,FileProtocolHandler")
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
+}
