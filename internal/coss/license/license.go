005a7b30198f7e57f2162a0d1f5c624aab0f47db
Mark Phelps <209477+markphelps@users.noreply.github.com>
2025-07-23 13:06:49 -0400

    chore: move shutdown ctx to main
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- internal/coss/license/license.go
+++ internal/coss/license/license.go
@@ -196,10 +196,7 @@ func (lm *ManagerImpl) Shutdown(ctx context.Context) error {
 			return err
 		}
 		// deactivate the license for this machine so it can be used on another machine
-		// Use a dedicated timeout context for deactivation to avoid competing with other shutdown operations
-		deactivateCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-		defer cancel()
-		if err := lm.license.Deactivate(deactivateCtx, fingerprint); err != nil {
+		if err := lm.license.Deactivate(ctx, fingerprint); err != nil {
 			lm.logger.Warn("failed to deactivate license", zap.Error(err))
 		}
 	}
