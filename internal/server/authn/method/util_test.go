9e41bea7f623c4d03c4fa9d6706c88ae82e29a3f
tejasvini1 <33099251+ptejasvini@users.noreply.github.com>
2025-10-03 18:31:26 +0000

    Fixed UT failures
    Signed-off-by: tejasvini1 <33099251+ptejasvini@users.noreply.github.com>

--- internal/server/authn/method/util_test.go
+++ internal/server/authn/method/util_test.go
@@ -12,27 +12,31 @@ import (
 func TestCallbackValidateState(t *testing.T) {
 	t.Run("missing state parameter", func(t *testing.T) {
 		err := CallbackValidateState(context.Background(), "")
-		require.ErrorIs(t, err, errors.ErrUnauthenticated)
+		var unauthErr errors.ErrUnauthenticated
+		require.ErrorAs(t, err, &unauthErr)
 		require.Contains(t, err.Error(), "missing state parameter")
 	})
 
 	t.Run("missing metadata parameter", func(t *testing.T) {
 		err := CallbackValidateState(context.Background(), "state123")
-		require.ErrorIs(t, err, errors.ErrUnauthenticated)
+		var unauthErr errors.ErrUnauthenticated
+		require.ErrorAs(t, err, &unauthErr)
 		require.Contains(t, err.Error(), "missing metadata parameter")
 	})
 
 	t.Run("missing client state in metadata", func(t *testing.T) {
 		ctx := metadata.NewIncomingContext(context.Background(), metadata.MD{})
 		err := CallbackValidateState(ctx, "state123")
-		require.ErrorIs(t, err, errors.ErrUnauthenticated)
+		var unauthErr errors.ErrUnauthenticated
+		require.ErrorAs(t, err, &unauthErr)
 		require.Contains(t, err.Error(), "missing client state in metadata")
 	})
 
 	t.Run("state mismatch", func(t *testing.T) {
 		ctx := metadata.NewIncomingContext(context.Background(), metadata.Pairs("flipt_client_state", "otherstate"))
 		err := CallbackValidateState(ctx, "state123")
-		require.ErrorIs(t, err, errors.ErrUnauthenticated)
+		var unauthErr errors.ErrUnauthenticated
+		require.ErrorAs(t, err, &unauthErr)
 		require.Contains(t, err.Error(), "unexpected state parameter")
 	})
 
